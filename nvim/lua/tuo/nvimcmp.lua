11bf286992b43e472260c37a31affaa2c4746b70
youngtuotuo <youngtuotuo@protonmail.com>
2022-11-02 19:38:19 +0800

    update

--- nvim/lua/tuo/nvimcmp.lua
+++ nvim/lua/tuo/nvimcmp.lua
@@ -34,7 +34,7 @@ M.setup = function()
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       -- ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
--- nvim/lua/tuo/packer.lua
+++ nvim/lua/tuo/packer.lua
@@ -205,11 +205,13 @@ return require('packer').startup(function()
     config = function()
       vim.cmd [[
         hi IndentBlanklineContextStart cterm=nocombine gui=nocombine
+        hi IndentBlanklineContextChar guifg=LightGrey
       ]]
       require("indent_blankline").setup {
         space_char_blankline = " ",
         show_current_context = true,
-        show_current_context_start = true,
+        show_current_context_start = false,
+        show_first_indent_level = false,
         enabled = true,
         show_end_of_line = true
     }
