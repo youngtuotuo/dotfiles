03c610a234f3a140f416379456c4e97fdc8dd0e9
youngtuotuo <youngtuotuo@protonmail.com>
2023-01-16 15:13:27 +0800

    simple lua line

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -3,12 +3,9 @@ require("onedark").setup({
     ending_tildes = true,
     cmp_itemkind_reverse = true,
     lualine = {
-        transparent = true, -- lualine center bar transparency
+        transparent = true -- lualine center bar transparency
     },
-    diagnostics = {
-        undercurl = true,
-        background = true,
-    }
+    diagnostics = {undercurl = true, background = true}
 })
 require("onedark").load()
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", {undercurl = true})
@@ -17,78 +14,62 @@ vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", {undercurl = true})
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", {undercurl = true})
 
 local config = {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = {left = '', right = ''},
+        section_separators = {left = '', right = ''},
+        disabled_filetypes = {statusline = {}, winbar = {}},
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = true,
+        refresh = {statusline = 1000, tabline = 1000, winbar = 1000}
     },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = true,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {
-        {
-            'mode',
-            fmt = function(string) return "--" .. string .. "--" end
+    sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+            {'mode'}, {'%='},
+            {'filetype', colored = true, icon_only = true, icon = {align = 'left'}, padding = {left = 0}},
+            {'filename', path = 1}, {
+                'lsp_progress',
+                display_components = {'spinner'},
+                spinner_symbols = {'⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷'}
+            }
         },
-    },
-    lualine_b = {},
-    lualine_c = {{'%='},
-        {
-            'filetype',
-            colored = true,
-            icon_only = true,
-            icon = {align='left'},
-            padding = {left=0}
+        lualine_x = {
+            {
+                function()
+                    local msg = 'No Active Lsp'
+                    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+                    local clients = vim.lsp.get_active_clients()
+                    if next(clients) == nil then return msg end
+                    for _, client in ipairs(clients) do
+                        local filetypes = client.config.filetypes
+                        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                            return client.name
+                        end
+                    end
+                    return msg
+                end,
+                icon = ' '
+            }, {'location'}
         },
-        {'filename', path=1},
-        {
-            'lsp_progress',
-            display_components = {'spinner'},
-            spinner_symbols = { '⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷' },
-        }
+        lualine_y = {},
+        lualine_z = {}
     },
-    lualine_x = {
-        {
-            function()
-                local msg = 'No Active Lsp'
-                local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-                local clients = vim.lsp.get_active_clients()
-                if next(clients) == nil then return msg end
-                for _, client in ipairs(clients) do
-                    local filetypes = client.config.filetypes
-                    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then return client.name end
-                end
-                return msg
-            end,
-            icon = ' ',
-        }
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {{'%='}, {'filename', path = 1}},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
     },
-    lualine_y = { },
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {{'%='}, {'filename', path=1}},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
 }
 
 require('lualine').setup(config)
