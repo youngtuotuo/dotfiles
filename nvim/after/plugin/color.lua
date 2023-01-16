bd60b50cb552085eb02afdc45724b189748a75c5
youngtuotuo <youngtuotuo@protonmail.com>
2023-01-16 15:04:28 +0800

    laststatus

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -28,7 +28,7 @@ local config = {
     },
     ignore_focus = {},
     always_divide_middle = true,
-    globalstatus = false,
+    globalstatus = true,
     refresh = {
       statusline = 1000,
       tabline = 1000,
