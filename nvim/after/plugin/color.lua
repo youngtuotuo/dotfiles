2a58781485efe048680494bbe0e01a9777ce2789
Tuo Hung <youngtuotuo@protonmail.com>
2023-04-06 18:34:28 +0800

    update

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -7,7 +7,7 @@ require('kanagawa').setup({
     keywordStyle = { italic = true },
     statementStyle = { bold = true },
     typeStyle = { italic = true},
-    transparent = true,         -- do not set background color
+    transparent = false,         -- do not set background color
     dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
     terminalColors = true,       -- define vim.g.terminal_color_{0,17}
     colors = {                   -- add/modify theme and palette colors
