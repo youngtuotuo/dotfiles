403c250f9c8165dcc957a2f48d54fa2992474ab9
youngtuotuo <youngtuotuo@protonmail.com>
2023-04-07 14:25:38 +0800

    function style bold

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -3,10 +3,10 @@ require('kanagawa').setup({
     compile = false,             -- enable compiling the colorscheme
     undercurl = true,            -- enable undercurls
     commentStyle = { italic = false },
-    functionStyle = { italic = true},
-    keywordStyle = { italic = true },
-    statementStyle = { bold = true },
-    typeStyle = { italic = true},
+    functionStyle = { bold = false, italic = false},
+    keywordStyle = { bold = true, italic = false },
+    statementStyle = { bold = false },
+    typeStyle = { italic = false},
     transparent = false,         -- do not set background color
     dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
     terminalColors = true,       -- define vim.g.terminal_color_{0,17}
