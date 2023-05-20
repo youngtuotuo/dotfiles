63053120b3c5f4458cb48c095c84cbd57cc59f43
Tuo Hung <youngtuotuo@protonmail.com>
2023-05-21 00:23:09 +0800

    refactor

--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -38,33 +38,6 @@ if next(vim.api.nvim_list_uis()) ~= nil then
               return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
             end,
         },
-        -- view = {
-        --     float = {
-        --         enable = false,
-        --         open_win_config = function()
-        --           local screen_w = vim.opt.columns:get()
-        --           local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-        --           local window_w = screen_w * WIDTH_RATIO
-        --           local window_h = screen_h * HEIGHT_RATIO
-        --           local window_w_int = math.floor(window_w)
-        --           local window_h_int = math.floor(window_h)
-        --           local center_x = (screen_w - window_w) / 2
-        --           local center_y = ((vim.opt.lines:get() - window_h) / 2)
-        --                            - vim.opt.cmdheight:get()
-        --           return {
-        --             border = 'rounded',
-        --             relative = 'editor',
-        --             row = center_y,
-        --             col = center_x,
-        --             width = window_w_int,
-        --             height = window_h_int,
-        --           }
-        --         end,
-        --     },
-        --     width = function()
-        --       return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
-        --     end,
-        -- }
     })
     vim.api.nvim_create_user_command('E', 'NvimTreeToggle', {})
     local function open_nvim_tree(data)
