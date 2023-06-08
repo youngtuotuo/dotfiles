5096028f5b3974556612bc07f012c41730367aae
youngtuotuo <youngtuotuo@protonmail.com>
2023-06-08 16:05:25 +0800

    remove new line char

--- nvim/after/plugin/indent-blankline.lua
+++ nvim/after/plugin/indent-blankline.lua
@@ -1,12 +1,12 @@
-vim.opt.list = true
-vim.opt.listchars = "eol:⏎"
+-- vim.opt.list = true
+-- vim.opt.listchars = "eol:⏎"
 require("indent_blankline").setup {
     enabled = true,
     space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = false,
     show_first_indent_level = false,
-    show_end_of_line = true,
+    show_end_of_line = false,
     show_trailing_blankline_indent = false,
 }
 
