189a99926e240be3da09f21594ae558366c9338a
Tuo Hung <youngtuotuo@protonmail.com>
2023-06-15 23:33:17 +0800

    disable treesitter on bash

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -6,7 +6,7 @@ require("nvim-treesitter.configs").setup({
     indent = {enable = false},
     highlight = {
         enable = true, -- false will disable the whole extension
-        disable = {"latex"}
+        disable = {"latex", "bash"}
     },
     textobjects = {
         enable = true,
