08a595ee215484de56c4e386332200df0edb17c6
youngtuotuo <youngtuotuo@protonmail.com>
2023-06-19 14:41:00 +0800

    bouncing ball

--- nvim/after/plugin/fidget.lua
+++ nvim/after/plugin/fidget.lua
@@ -1,17 +1,6 @@
 require('fidget').setup({
     text = {
-        spinner = { -- bouncingBall
-            "( ●    )",
-            "(  ●   )",
-            "(   ●  )",
-            "(    ● )",
-            "(     ●)",
-            "(    ● )",
-            "(   ●  )",
-            "(  ●   )",
-            "( ●    )",
-            "(●     )",
-        },         -- animation shown when tasks are ongoing
+        spinner = "bouncing_ball",         -- animation shown when tasks are ongoing
         done = "✔",               -- character shown when all tasks are complete
         commenced = "Started",    -- message shown when task starts
         completed = "Completed",  -- message shown when task completes
