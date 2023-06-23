5c810e97bde021dd3cd8c507aa67aff7cc5b4768
Tuo Hung <youngtuotuo@protonmail.com>
2023-06-23 12:55:17 +0800

    telescope adjust

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,13 +5,12 @@ local actions = require 'telescope.actions'
 
 require("telescope").setup({
     defaults = {
-        layout_strategy = 'horizontal',
         layout_config = {
-            anchor = "S",
-            height = 0.5,
-            width = 0.85,
-            preview_width = 0.5,
-            prompt_position = 'top',
+            horizontal = {
+                height = 0.7,
+                width = 0.85,
+                preview_width = 0.5,
+            }
         },
         color_devicons = true,
         prompt_prefix = "> ",
@@ -51,14 +50,14 @@ pcall(require('telescope').load_extension, 'fzf')
 
 local default_opts = {noremap = true, silent = true}
 -- Telescope Stuff
-vim.api.nvim_set_keymap("n", "<space>r", ":Telescope lsp_references<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<space>e", ":Telescope find_files<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<space>f", ":Telescope current_buffer_fuzzy_find<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<space>g", ":Telescope git_files<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<space>d", ":Telescope diagnostics<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<space>l", ":Telescope live_grep<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<space>b", ":Telescope buffers<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<space>c", ":Telescope commands<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<space>h", ":Telescope help_tags<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<space>m", ":Telescope keymaps<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<space>v", ":Telescope lsp_document_symbols<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<space>r", ":lua require('telescope.builtin').lsp_references()<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<space>e", ":lua require('telescope.builtin').find_files()<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<space>f", ":lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<space>g", ":lua require('telescope.builtin').git_files()<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<space>d", ":lua require('telescope.builtin').diagnostics()<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<space>l", ":lua require('telescope.builtin').live_grep()<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<space>b", ":lua require('telescope.builtin').buffers()<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<space>c", ":lua require('telescope.builtin').commands()<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<space>h", ":lua require('telescope.builtin').help_tags()<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<space>m", ":lua require('telescope.builtin').keymaps()<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<space>v", ":lua require('telescope.builtin').lsp_document_symbols()<CR>", default_opts)
