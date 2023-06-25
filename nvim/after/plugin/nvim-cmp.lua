2f41b384548d6d5b657bf6d20dbbecc7080b2cc9
youngtuotuo <youngtuotuo@protonmail.com>
2023-06-25 14:53:46 +0800

    <c-l> for cmp select

--- nvim/after/plugin/nvim-cmp.lua
+++ nvim/after/plugin/nvim-cmp.lua
@@ -63,7 +63,7 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         -- Accept currently selected item.
         -- Set `select` to `false` to only confirm explicitly selected items.
-        -- ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ['<C-l>'] = cmp.mapping.confirm({select = false}),
         ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
         ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s"})
     }),
