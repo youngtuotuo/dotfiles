29dfe5994e2e36278292d49d01f2aed49eed1910
youngtuotuo <youngtuotuo@protonmail.com>
2023-07-06 13:59:00 +0800

    cmp witdh=40

--- nvim/after/plugin/nvim-cmp.lua
+++ nvim/after/plugin/nvim-cmp.lua
@@ -15,8 +15,8 @@ local has_words_before = function()
 end
 
 local ELLIPSIS_CHAR = '…'
-local MAX_LABEL_WIDTH = 20
-local MIN_LABEL_WIDTH = 20
+local MAX_LABEL_WIDTH = 40
+local MIN_LABEL_WIDTH = 40
 cmp.setup({
     -- completion = {autocomplete = true},
     window = {
