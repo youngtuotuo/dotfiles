8d58ca659fc5c5fbb1dad6f3776c797e90c61a99
youngtuotuo <youngtuotuo@protonmail.com>
2023-07-27 15:30:29 +0800

    harpoon

--- nvim/lua/tuo/lazy.lua
+++ nvim/lua/tuo/lazy.lua
@@ -296,6 +296,21 @@ local plugins = {
             'debugloop/telescope-undo.nvim',
         }
     },
+    {
+        'theprimeagen/harpoon',
+        event = 'VimEnter',
+        keys = {
+            {"<leader>a", function() require('harpoon.mark').add_file() end, mode="n"},
+            {"<C-e>", function() require('harpoon.ui').toggle_quick_menu() end, mode="n"},
+            {"<M-1>", function() require('harpoon.ui').nav_file(1) end, mode="n"},
+            {"<M-2>", function() require('harpoon.ui').nav_file(2) end, mode="n"},
+            {"<M-3>", function() require('harpoon.ui').nav_file(3) end, mode="n"},
+            {"<M-4>", function() require('harpoon.ui').nav_file(4) end, mode="n"},
+        },
+        config = function()
+            require('lazyload.harpoon')
+        end
+    },
 }
 
 local opts = {
