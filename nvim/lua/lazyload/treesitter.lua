0dc72c31b9f1b09c6a66153aad4e1d95e650d7ad
Tuo Hung <youngtuotuo@protonmail.com>
2023-07-30 18:35:26 +0800

    asm not work

--- nvim/lua/lazyload/treesitter.lua
+++ nvim/lua/lazyload/treesitter.lua
@@ -1,13 +1,5 @@
-require('nvim-treesitter.parsers').get_parser_configs().asm = {
-    install_info = {
-        url = 'https://github.com/rush-rs/tree-sitter-asm.git',
-        files = { 'src/parser.c' },
-        branch = 'main',
-    },
-}
 require("nvim-treesitter.configs").setup({
   ensure_installed = {
-    "asm",
     "markdown",
     "bash",
     "c",
