76722ac3cc30cef8ef8123f99915be10f74500de
youngtuotuo <youngtuotuo@protonmail.com>
2023-08-02 12:26:16 +0800

    external command output shortcut

--- nvim/lua/tuo/keymaps.lua
+++ nvim/lua/tuo/keymaps.lua
@@ -92,3 +92,7 @@ keymap("i", "<Tab>", "<C-i>", default_opts)
 -- better search and replace
 keymap("n", "<space>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 keymap("n", "<space>x", "<cmd>!chmod +x %<cr>", default_opts)
+
+-- better external command ouput
+keymap("n", "<leader>x", [[:sp | terminal ]])
+
