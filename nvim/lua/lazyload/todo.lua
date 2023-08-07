7fdcefd228b02cbf16e9d485a8fb174d1854c794
youngtuotuo <youngtuotuo@protonmail.com>
2023-08-07 11:30:34 +0800

    todo

--- nvim/lua/lazyload/todo.lua
+++ nvim/lua/lazyload/todo.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_set_keymap("n", "<space>t", ":TodoTelescope<CR>", { noremap = true, silent = true })
 
 require("todo-comments").setup({
-  signs = true, -- show icons in the signs column
+  signs = false, -- show icons in the signs column
   sign_priority = 1, -- sign priority
   -- keywords recognized as todo comments
   keywords = {
@@ -20,7 +20,7 @@ require("todo-comments").setup({
   },
   gui_style = {
     fg = "NONE", -- The gui style to use for the fg highlight group.
-    bg = "BOLD", -- The gui style to use for the bg highlight group.
+    bg = "NONE", -- The gui style to use for the bg highlight group.
   },
   merge_keywords = true, -- when true, custom keywords will be merged with the defaults
   -- highlighting of the line containing the todo comment
