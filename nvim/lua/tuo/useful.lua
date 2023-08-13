9785c9e7be0e074c1c21e77552e62f171ac3e81a
Tuo Hung <youngtuotuo@protonmail.com>
2023-08-13 11:38:54 +0800

    local ip in win

--- nvim/lua/tuo/useful.lua
+++ nvim/lua/tuo/useful.lua
@@ -15,7 +15,17 @@ TRANS = true
 COLORSCHEME = "tokyonight" -- "tokyonight", "kanagawa"
 
 GET_LAN_IP = function()
+  if vim.fn.has("win32") == 1 then
+    local ipconfig_command = io.popen("ipconfig")
+    local output = ipconfig_command:read("*a")
+    ipconfig_command:close()
+
+    local lan_ip = output:match("IPv4 Address%s*:%s*([%d%.]+)")
+
+    return lan_ip
+  else
     local cmd = "ip route get 1.1.1.1 | awk '{print $7}'"
     local ip = vim.fn.system(cmd)
     return ip:gsub("%s+", "")  -- Remove any leading/trailing whitespace
+  end
 end
