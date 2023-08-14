465cf642ed8338b844abd82d830e7b76a57e42bb
Tuo Hung <youngtuotuo@protonmail.com>
2023-08-14 22:36:59 +0800

    update c, cpp execute command

--- nvim/after/ftplugin/c.lua
+++ nvim/after/ftplugin/c.lua
@@ -9,6 +9,6 @@ if vim.fn.has("win32") == 1 then
   sep = "\\"
   compiler = "gcc"
 end
-local cmd = ":13sp | execute 'terminal " .. compiler .. " -Wall -o " .. fname_next .. ext .. " %"
-cmd = cmd .. " && ." .. sep .. fname_next .. ext .. "'"
+local cmd = ":13sp | terminal " .. compiler .. " -Wall -o " .. fname_next .. ext .. " %"
+cmd = cmd .. " && ." .. sep .. fname_next .. ext
 vim.keymap.set("n", "<leader>p", cmd)
