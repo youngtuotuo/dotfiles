324092d67bee1799a7484c58d245f7417e48a9cb
youngtuotuo <youngtuotuo@protonmail.com>
2023-08-14 23:00:06 +0800

    automatically add -lm based on the header file

--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -1,5 +1,13 @@
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
+local check_math_h = function()
+  local content = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  if vim.fn.match(content, "math.h") == -1 then
+    return false
+  end
+  return true
+
+end
 local fname_next = vim.fn.expand("%:t:r")
 local ext = ""
 local sep = "/"
@@ -9,6 +17,10 @@ if vim.fn.has("win32") == 1 then
   sep = "\\"
   compiler = "g++"
 end
-local cmd = ":13sp | terminal " .. compiler .. " -Wall -std=c++14 -o " .. fname_next .. ext .. " %"
+local cmd = ":13sp | terminal " .. compiler .. " -Wall -std=c++14 "
+if check_math_h() then
+  cmd = cmd .. "-lm "
+end
+cmd = cmd .. "-o " .. fname_next .. ext .. " %"
 cmd = cmd .. " && ." .. sep .. fname_next .. ext
 vim.keymap.set("n", "<leader>p", cmd)
