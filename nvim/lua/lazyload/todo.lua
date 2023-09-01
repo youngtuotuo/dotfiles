0f252d0099ae060f50c3f7a4f343d1f070e81b18
youngtuotuo <youngtuotuo@protonmail.com>
2023-09-01 19:41:46 +0800

    todo update

--- nvim/lua/lazyload/todo.lua
+++ nvim/lua/lazyload/todo.lua
@@ -20,7 +20,7 @@ require("todo-comments").setup({
   },
   gui_style = {
     fg = "NONE", -- The gui style to use for the fg highlight group.
-    bg = "NONE", -- The gui style to use for the bg highlight group.
+    bg = "BOLD", -- The gui style to use for the bg highlight group.
   },
   merge_keywords = true, -- when true, custom keywords will be merged with the defaults
   -- highlighting of the line containing the todo comment
@@ -32,8 +32,8 @@ require("todo-comments").setup({
     multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
     multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
     before = "", -- "fg" or "bg" or empty
-    keyword = "fg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-    after = "", -- "fg" or "bg" or empty
+    keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+    after = "fg", -- "fg" or "bg" or empty
     pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
     comments_only = true, -- uses treesitter to match keywords in comments only
     max_line_len = 400, -- ignore lines longer than this
@@ -60,6 +60,6 @@ require("todo-comments").setup({
     },
     -- regex that will be used to match keywords.
     -- don't replace the (KEYWORDS) placeholder
-    pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+    pattern = [[\s\b(KEYWORDS):]], -- ripgrep regex
   },
 })
