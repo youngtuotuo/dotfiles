ea5e398da7c86221b81a3fe854ff59ac813038b0
youngtuotuo <youngtuotuo@protonmail.com>
2023-09-01 16:23:28 +0800

    udpate

--- nvim/lua/lazyload/treesitter.lua
+++ nvim/lua/lazyload/treesitter.lua
@@ -21,7 +21,7 @@ require("nvim-treesitter.configs").setup({
   indent = { enable = true },
   auto_install = false,
   highlight = {
-    enable = false, -- false will disable the whole extension
+    enable = true, -- false will disable the whole extension
     disable = { "latex", "bash", "zig", "markdown" },
   },
   textobjects = {
