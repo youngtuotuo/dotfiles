11ee60a19812ec37dfdb0dfc4c8126f04fff423e
youngtuotuo <youngtuotuo@protonmail.com>
2023-09-08 14:59:40 +0800

    sudiafasd

--- nvim/lua/lazyload/todo.lua
+++ nvim/lua/lazyload/todo.lua
@@ -18,10 +18,10 @@ require("todo-comments").setup({
     TODO = { icon = " ", color = "hint" },
     HACK = { icon = " ", color = "error" },
     WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-    PERF = { icon = " ", color = "default", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+    PERF = { icon = "󰖷 ", color = "default", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
     NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
     TEST = { icon = " ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-    IDEA = { icon = " ", color = "test", alt = { "TRY", "HINT", "THINK" } },
+    IDEA = { icon = " ", color = "test", alt = { "TRY", "HINT", "THINK" } },
   },
   gui_style = {
     fg = "NONE", -- The gui style to use for the fg highlight group.
