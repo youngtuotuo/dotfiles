399e47cccdda334d00c01c33e687d50e80f50886
youngtuotuo <youngtuotuo@protonmail.com>
2023-10-01 01:13:47 +0800

    wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,9 +1,23 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
 
+local function getOS()
+  -- ask LuaJIT first
+  if jit then
+    return jit.os
+  end
+
+  -- Unix, Linux variants
+  local fh, err = assert(io.popen("uname -o 2>/dev/null", "r"))
+  if fh then
+    osname = fh:read()
+  end
+
+  return osname or "Windows"
+end
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
@@ -12,9 +26,9 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font 'CaskaydiaCove Nerd Font'
+config.font = wezterm.font("CaskaydiaCove Nerd Font")
 config.font_size = 15
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.window_background_image_hsb = {
   -- Darken the background image by reducing it to 1/3rd
   brightness = 0.02,
@@ -30,28 +44,35 @@ config.hide_tab_bar_if_only_one_tab = true
 config.selection_word_boundary = " \t\n{}[]()\"'`@.,;:"
 config.mouse_bindings = {
   {
-    event = { Down = { streak = 3, button = 'Left' } },
-    action = wezterm.action.SelectTextAtMouseCursor 'SemanticZone',
-    mods = 'NONE',
+    event = { Down = { streak = 3, button = "Left" } },
+    action = wezterm.action.SelectTextAtMouseCursor("SemanticZone"),
+    mods = "NONE",
   },
   {
-    event = { Up = { streak = 1, button = 'Left' } },
-    mods = 'SHIFT',
-    action = wezterm.action.ExtendSelectionToMouseCursor 'Word',
+    event = { Up = { streak = 1, button = "Left" } },
+    mods = "SHIFT",
+    action = wezterm.action.ExtendSelectionToMouseCursor("Word"),
   },
 }
 
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
 config.cursor_blink_rate = 800
-config.color_scheme = 'Dark Pastel'
+config.color_scheme = "Dark Pastel"
 
 config.tab_bar_at_bottom = true
 
 local dimmer = { brightness = 0.1 }
-local file = os.getenv('HOME') .. '/github/dotfiles/pictures/future-buildings-minimal-ve.jpg'
+local file = ""
+if os.getenv("HOME") then
+  file = os.getenv("HOME") .. "/github/dotfiles/pictures/future-buildings-minimal-ve.jpg"
+end
+if getOS() == "Windows" then
+  file = "C:/Users/User/github/dotfiles/pictures/future-buildings-minimal-ve.jpg"
+  config.default_domain = 'WSL:Ubuntu-20.04'
+end
 config.background = {
-    {
+  {
     source = {
       File = file,
     },
