c51bb8d689eed0b55f1b18f8e1caff7d7239923d
youngtuotuo <youngtuotuo@protonmail.com>
2023-09-30 11:04:14 +0800

    wueirbczvk

--- nvim/lua/lazyload/indent-blankline.lua
+++ nvim/lua/lazyload/indent-blankline.lua
@@ -1,12 +1,4 @@
--- vim.opt.list = true
--- vim.opt.listchars = "eol:⏎"
 require("ibl").setup({
   enabled = false,
-  space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = false,
-  show_first_indent_level = false,
-  show_end_of_line = false,
-  show_trailing_blankline_indent = false,
 })
 vim.keymap.set("n", "<leader>i", ":IBLToggle<cr>")
--- nvim/lua/tuo/options.lua
+++ nvim/lua/tuo/options.lua
@@ -145,20 +145,22 @@ vim.api.nvim_create_user_command("Qa", "qa", {})
 
 -- Cursorline highlighting control
 --  Only have it on in the active buffer
-vim.opt.cursorline = true -- Highlight the current line
-local group = vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
-local set_cursorline = function(event, value, pattern)
-  vim.api.nvim_create_autocmd(event, {
-    group = group,
-    pattern = pattern,
-    callback = function()
-      vim.opt_local.cursorline = value
-    end,
-  })
-end
-set_cursorline("WinLeave", false)
-set_cursorline("WinEnter", true)
-set_cursorline("FileType", false, "TelescopePrompt")
+vim.opt.cursorline = false -- Highlight the current line
+-- local group = vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
+-- local set_cursorline = function(event, value, pattern)
+--   vim.api.nvim_create_autocmd(event, {
+--     group = group,
+--     pattern = pattern,
+--     callback = function()
+--       vim.opt_local.cursorline = value
+--     end,
+--   })
+-- end
+-- set_cursorline("WinLeave", false)
+-- set_cursorline("WinEnter", true)
+-- set_cursorline("FileType", false, "TelescopePrompt")
+
+-- mojo filetype
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = { "*.🔥", "*.mojo" },
   callback = function()
