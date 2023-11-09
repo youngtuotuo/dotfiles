52b2069ec000fdb6ce4ea6bb207227f962a17ed7
Tuo Hung <youngtuotuo@protonmail.com>
2023-11-09 21:46:01 +0800

    oijasdf

--- nvim/lua/lazyload/lspconfig.lua
+++ nvim/lua/lazyload/lspconfig.lua
@@ -115,13 +115,13 @@ vim.api.nvim_create_autocmd("LspAttach", {
       callback = function()
         pcall(vim.api.nvim_buf_clear_namespace, args.buf, ns, 0, -1)
         local hi = { "Error", "Warn", "Info", "Hint" }
-        local icons = { " ", " ", " ", " " }
+        -- local icons = { " ", " ", " ", " " }
         local curline = vim.api.nvim_win_get_cursor(0)[1]
         local diagnostics = vim.diagnostic.get(args.buf, { lnum = curline - 1 })
         local virt_texts = { { (" "):rep(4) } }
         for _, diag in ipairs(diagnostics) do
           virt_texts[#virt_texts + 1] =
-            { icons[diag.severity] .. diag.message, "Diagnostic" .. hi[diag.severity] }
+            { diag.message, "Diagnostic" .. hi[diag.severity] }
         end
         vim.api.nvim_buf_set_extmark(args.buf, ns, curline - 1, 0, {
           virt_text = virt_texts,
