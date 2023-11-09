a9ce1cfb228bfccd267a84ce0fdbe27f97d0d883
Tuo Hung <youngtuotuo@protonmail.com>
2023-11-09 21:09:06 +0800

    aisopuf

--- nvim/lua/lazyload/lspconfig.lua
+++ nvim/lua/lazyload/lspconfig.lua
@@ -136,8 +136,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 local signs = {
   { name = "DiagnosticSignError", text = "" },
   { name = "DiagnosticSignWarn", text = "" },
-  { name = "DiagnosticSignHint", text = "" },
-  { name = "DiagnosticSignInfo", text = "" },
+  { name = "DiagnosticSignHint", text = "" },
+  { name = "DiagnosticSignInfo", text = "" },
 }
 
 for _, sign in ipairs(signs) do
