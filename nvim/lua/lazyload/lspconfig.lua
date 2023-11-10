3dce5e96db49ee811d651f948ba5686e4e0a8484
youngtuotuo <youngtuotuo@protonmail.com>
2023-11-10 17:36:18 +0800

    asdifuop

--- nvim/lua/lazyload/lspconfig.lua
+++ nvim/lua/lazyload/lspconfig.lua
@@ -70,7 +70,7 @@ require("mason-lspconfig").setup({ handlers = handlers })
 
 local diag_config = {
   virtual_text = false,
-  signs = true,
+  signs = false,
   underline = true,
   update_in_insert = false,
   severity_sort = true,
@@ -115,20 +115,22 @@ vim.api.nvim_create_autocmd("LspAttach", {
       callback = function()
         pcall(vim.api.nvim_buf_clear_namespace, args.buf, ns, 0, -1)
         local hi = { "Error", "Warn", "Info", "Hint" }
-        -- local icons = { " ", " ", " ", " " }
-        local curline = vim.api.nvim_win_get_cursor(0)[1]
+        local icons = { " ", " ", " ", " " }
+        local curline, curcol = unpack(vim.api.nvim_win_get_cursor(0))
         local diagnostics = vim.diagnostic.get(args.buf, { lnum = curline - 1 })
-        local virt_texts = { { (" "):rep(4) } }
-        for _, diag in ipairs(diagnostics) do
-          virt_texts[#virt_texts + 1] =
-            { diag.message, "Diagnostic" .. hi[diag.severity] }
+          local virt_texts = { { (" "):rep(4) } }
+          for _, diag in ipairs(diagnostics) do
+            if curcol >= diag.col and curcol < diag.end_col  then
+              virt_texts[#virt_texts + 1] =
+                { icons[diag.severity] .. diag.message, "Diagnostic" .. hi[diag.severity] }
+            end
+          end
+          vim.api.nvim_buf_set_extmark(args.buf, ns, curline - 1, 0, {
+            virt_text = virt_texts,
+            hl_mode = "combine",
+            virt_text_pos = "eol"
+          })
         end
-        vim.api.nvim_buf_set_extmark(args.buf, ns, curline - 1, 0, {
-          virt_text = virt_texts,
-          hl_mode = "combine",
-          virt_text_pos = "eol"
-        })
-      end,
     })
   end,
 })
