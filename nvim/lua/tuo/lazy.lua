01f54a20c1945e7e18af4dd64283d94fe246fa5a
youngtuotuo <youngtuotuo@protonmail.com>
2023-11-10 09:46:54 +0800

    saiofusadp[

--- nvim/lua/tuo/lazy.lua
+++ nvim/lua/tuo/lazy.lua
@@ -40,23 +40,6 @@ local plugins = {
     },
   },
   {
-    "stevearc/oil.nvim",
-    opts = {},
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("oil").setup({
-        keymaps = {
-          ["<C-h>"] = "actions.select_vsplit",
-          ["<C-s>"] = "actions.select_split",
-        },
-        view_options = {
-          show_hidden = true,
-        },
-      })
-    end,
-  },
-  {
     "theprimeagen/harpoon",
     config = function()
       require("lazyload.harpoon")
