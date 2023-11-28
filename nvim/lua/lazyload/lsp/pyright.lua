9fbe9dd3485656fef72ebf734543f926e424f25a
youngtuotuo <youngtuotuo@protonmail.com>
2023-11-28 17:08:30 +0800

    fiaopf

--- nvim/lua/lazyload/lsp/pyright.lua
+++ nvim/lua/lazyload/lsp/pyright.lua
@@ -1,4 +1,5 @@
 local g = require("tuo.global")
+local util = require('vim.lsp.util')
 local function filter(arr, func)
   -- Filter in place
   -- https://stackoverflow.com/questions/49709998/how-to-filter-a-lua-array-inplace
@@ -41,19 +42,88 @@ local function custom_on_publish_diagnostics(a, params, client_id, c, config)
   vim.lsp.diagnostic.on_publish_diagnostics(a, params, client_id, c, config)
 end
 
+local function split_lines(value)
+  value = string.gsub(value, '\r\n?', '\n')
+  value = string.gsub(value, '&nbsp;', ' ')
+  value = string.gsub(value, '\\', '')
+  value = string.gsub(value, '_', '')
+  return vim.split(value, '\n', { plain = true, trimempty = true })
+end
+
+local function convert_input_to_markdown_lines(input, contents)
+  contents = contents or {}
+  -- MarkedString variation 1
+  if type(input) == 'string' then
+    vim.list_extend(contents, split_lines(input))
+  else
+    assert(type(input) == 'table', 'Expected a table for LSP input')
+    -- MarkupContent
+    if input.kind then
+      local value = input.value or ''
+      vim.list_extend(contents, split_lines(value))
+      -- MarkupString variation 2
+    elseif input.language then
+      table.insert(contents, '```' .. input.language)
+      vim.list_extend(contents, split_lines(input.value or ''))
+      table.insert(contents, '```')
+      -- By deduction, this must be MarkedString[]
+    else
+      -- Use our existing logic to handle MarkedString
+      for _, marked_string in ipairs(input) do
+        convert_input_to_markdown_lines(marked_string, contents)
+      end
+    end
+  end
+  if (contents[1] == '' or contents[1] == nil) and #contents == 1 then
+    return {}
+  end
+  return contents
+end
+
+local function hover(_, result, ctx, config)
+  config = config or {}
+  config.focus_id = ctx.method
+  if vim.api.nvim_get_current_buf() ~= ctx.bufnr then
+    -- Ignore result since buffer changed. This happens for slow language servers.
+    return
+  end
+  if not (result and result.contents) then
+    if config.silent ~= true then
+      vim.notify('No information available')
+    end
+    return
+  end
+  local format = 'markdown'
+  local contents ---@type string[]
+  if type(result.contents) == 'table' and result.contents.kind == 'plaintext' then
+    format = 'plaintext'
+    contents = vim.split(result.contents.value or '', '\n', { trimempty = true })
+  else
+    contents = convert_input_to_markdown_lines(result.contents)
+  end
+  if vim.tbl_isempty(contents) then
+    if config.silent ~= true then
+      vim.notify('No information available')
+    end
+    return
+  end
+  return util.open_floating_preview(contents, format, config)
+end
+
+
 local config = function(capabilities, util)
   return {
     handlers = {
       ["textDocument/publishDiagnostics"] = vim.lsp.with(custom_on_publish_diagnostics, {}),
-      ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+      ["textDocument/hover"] = vim.lsp.with(hover, {
         border = g.border,
         title = " Pyright ",
-        max_width = 100,
+        -- max_width = 100,
         zindex = 500,
       }),
       ["textDocument/signatureHelp"] = vim.lsp.with(
         vim.lsp.handlers.signature_help,
-        { border = g.border, title = " PyRight ", max_width = 100 }
+        { border = g.border, title = " Pyright ", max_width = 100 }
       ),
     },
     capabilities = capabilities,
