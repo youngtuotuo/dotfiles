705c231089f4c50b6103ad36d326ea7f524adcf6
Tuo Hung <youngtuotuo@protonmail.com>
2023-12-03 00:02:33 +0800

    fiaosdupfuasd

--- nvim/lua/lazyload/gitsigns.lua
+++ nvim/lua/lazyload/gitsigns.lua
@@ -1,39 +1,14 @@
 local g = require("tuo.global")
 require("gitsigns").setup({
   signs = {
-    add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = {
-      hl = "GitSignsChange",
-      text = "~",
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn",
-    },
-    delete = {
-      hl = "GitSignsDelete",
-      text = "-",
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn",
-    },
-    topdelete = {
-      hl = "GitSignsDelete",
-      text = "-",
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn",
-    },
-    changedelete = {
-      hl = "GitSignsChange",
-      text = "~",
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn",
-    },
-    untracked = {
-      hl = "GitSignsAdd",
-      text = "+",
-      numhl = "GitSignsAddNr",
-      linehl = "GitSignsAddLn",
-    },
+    add          = { text = '│' },
+    change       = { text = '│' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
   },
-  signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
+  signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
   word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
