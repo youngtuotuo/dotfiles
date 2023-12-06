cdc331b0ce7415d3892ce4a547da676e30125f97
youngtuotuo <youngtuotuo@protonmail.com>
2023-12-06 11:40:20 +0800

    fasdfsd

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,17 +1,10 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-local act = wezterm.action
-act { SpawnCommandInNewTab = { cwd = wezterm.home_dir } }
 
 -- This table will hold the configuration.
 local config = {}
 
 local function getOS()
-  -- ask LuaJIT first
-  if jit then
-    return jit.os
-  end
-
   -- Unix, Linux variants
   local fh, err = assert(io.popen("uname -o 2>/dev/null", "r"))
   if fh then
@@ -43,7 +36,7 @@ else
 end
 -- This is where you actually apply your config choices
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 14
+config.font_size = 11
 config.adjust_window_size_when_changing_font_size = false
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.hide_tab_bar_if_only_one_tab = true
@@ -64,7 +57,6 @@ config.window_padding = {
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
 config.cursor_blink_rate = 800
-config.color_scheme = "Catppuccin Mocha"
 
 config.colors = {
   cursor_fg = "black",
