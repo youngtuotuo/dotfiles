a5e8644bd3b37b8b3d02ee4ef87c14ce36f667ff
youngtuotuo <youngtuotuo@protonmail.com>
2023-12-07 11:19:44 +0800

    fasdiofasd

--- .wezterm.lua
+++ .wezterm.lua
@@ -108,6 +108,8 @@ config.keys = {
     mods = 'CTRL|SHIFT',
     action = act.DetachDomain 'CurrentPaneDomain',
   },
+  { key = '{', mods = 'SHIFT|ALT', action = act.MoveTabRelative(-1) },
+  { key = '}', mods = 'SHIFT|ALT', action = act.MoveTabRelative(1) },
 }
 
 -- and finally, return the configuration to wezterm
