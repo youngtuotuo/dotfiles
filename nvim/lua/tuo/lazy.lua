806744c05d2fcb0caa59e30311341ed15e8422f0
youngtuotuo <youngtuotuo@protonmail.com>
2023-12-11 10:42:28 +0800

    fasdiofasd

--- nvim/lua/tuo/lazy.lua
+++ nvim/lua/tuo/lazy.lua
@@ -21,6 +21,15 @@ local plugins = {
       require("lazyload.colorscheme")
     end,
   },
+  {
+    'glacambre/firenvim',
+    -- Lazy load firenvim
+    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    lazy = not vim.g.started_by_firenvim,
+    build = function()
+      vim.fn["firenvim#install"](0)
+    end
+  },
   { "microsoft/python-type-stubs", cond = false },
   {
     "nvim-telescope/telescope.nvim",
