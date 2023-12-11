9bcea2006100e199546461b1a3a9e3e89f11dfed
youngtuotuo <youngtuotuo@protonmail.com>
2023-12-11 10:58:19 +0800

    oaufpsad

--- nvim/lua/lazyload/colorscheme.lua
+++ nvim/lua/lazyload/colorscheme.lua
@@ -1,14 +1,18 @@
 local transparent = "none"
 local fg = "#c8d3f5"
 local fg_dark = "#828bb8"
-local border_fg = fg_dark
 vim.api.nvim_set_hl(0, "Normal", { bg = transparent })
 vim.api.nvim_set_hl(0, "Error", { fg = transparent })
 vim.api.nvim_set_hl(0, "NormalFloat", { fg = fg, bg = transparent })
-vim.api.nvim_set_hl(0, "FloatBorder", { fg = border_fg, bg = transparent })
+vim.api.nvim_set_hl(0, "FloatBorder", { link = "NormalFloat" })
 vim.api.nvim_set_hl(0, "StatusLine", { fg = fg, bg = transparent })
 vim.api.nvim_set_hl(0, "StatusLineNC", { fg = fg_dark, bg = transparent })
 vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true })
+vim.api.nvim_set_hl(0, "DiagnosticFloatingOk", { fg = "NvimLightGreen", bg = transparent })
+vim.api.nvim_set_hl(0, "DiagnosticFloatingHint", { fg = "NvimLightBlue", bg = transparent })
+vim.api.nvim_set_hl(0, "DiagnosticFloatingInfo", { fg = "NvimLightCyan", bg = transparent })
+vim.api.nvim_set_hl(0, "DiagnosticFloatingWarn", { fg = "NvimLightYellow", bg = transparent })
+vim.api.nvim_set_hl(0, "DiagnosticFloatingError", { fg = "NvimLightRed", bg = transparent })
 vim.api.nvim_set_hl(0, "Pmenu", { link = "NormalFloat" })
 vim.api.nvim_set_hl(0, "CmpCompletionBorder", { link = "FloatBorder" })
 vim.api.nvim_set_hl(0, "CmpDocumentationBorder", { link = "FloatBorder" })
