dc99e304c1179be4fbaf816429999a20e90a7adf
youngtuotuo <youngtuotuo@protonmail.com>
2023-12-12 14:28:33 +0800

    iofasdpfupsa

--- nvim/lua/lazyload/colorscheme.lua
+++ nvim/lua/lazyload/colorscheme.lua
@@ -1,19 +1,25 @@
 local transparent = "none"
-local fg = "#c8d3f5"
-local fg_dark = "#828bb8"
+
 vim.api.nvim_set_hl(0, "Normal", { bg = transparent })
 vim.api.nvim_set_hl(0, "Error", { fg = transparent })
+
 vim.api.nvim_set_hl(0, "LspReferenceRead", { reverse = true })
 vim.api.nvim_set_hl(0, "LspReferenceWrite", { reverse = true })
 vim.api.nvim_set_hl(0, "Pmenu", { bg = transparent })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = transparent })
+
 vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true })
+
 vim.api.nvim_set_hl(0, "DiagnosticFloatingOk", { fg = "NvimLightGreen", bg = transparent })
 vim.api.nvim_set_hl(0, "DiagnosticFloatingHint", { fg = "NvimLightBlue", bg = transparent })
 vim.api.nvim_set_hl(0, "DiagnosticFloatingInfo", { fg = "NvimLightCyan", bg = transparent })
 vim.api.nvim_set_hl(0, "DiagnosticFloatingWarn", { fg = "NvimLightYellow", bg = transparent })
 vim.api.nvim_set_hl(0, "DiagnosticFloatingError", { fg = "NvimLightRed", bg = transparent })
+
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = transparent })
 vim.api.nvim_set_hl(0, "FloatBorder", { link = "NormalFloat" })
-vim.api.nvim_set_hl(0, "CmpCompletionBorder", { link = "FloatBorder" })
-vim.api.nvim_set_hl(0, "CmpDocumentationBorder", { link = "FloatBorder" })
-vim.api.nvim_set_hl(0, "VertSplit", { link = "SignColumn" })
+vim.api.nvim_set_hl(0, "CmpCompletionBorder", { link = "NormalFloat" })
+vim.api.nvim_set_hl(0, "CmpDocumentationBorder", { link = "NormalFloat" })
+
+vim.api.nvim_set_hl(0, "DiffAdd", { fg = "NvimLightGreen", bg = transparent })
+vim.api.nvim_set_hl(0, "DiffChange", { fg = "NvimLightYellow", bg = transparent })
+vim.api.nvim_set_hl(0, "DiffDelete", { fg = "NvimLightRed", bg = transparent })
