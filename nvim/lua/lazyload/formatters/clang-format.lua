7cb0008b60c03aa5b4b4052a2dec600147988b56
youngtuotuo <youngtuotuo@protonmail.com>
2023-12-16 13:55:16 +0800

    iofasdpfupsa

--- nvim/lua/lazyload/formatters/clang-format.lua
+++ nvim/lua/lazyload/formatters/clang-format.lua
@@ -1,7 +1,7 @@
 return function()
   return {
     exe = "clang-format",
-    args = { '--style="{BasedOnStyle: llvm, ColumnLimit: 100, IndentWidth: 2, AccessModifierOffset: -2}"' },
+    args = { '--style="{BasedOnStyle: llvm, ColumnLimit: 100, IndentWidth: 2, AccessModifierOffset: -2, PointerAlignment: Left}"' },
     stdin = true,
   }
 end
