9b96e3844035aa8a8588ecf4fd8162df66fdcfbb
youngtuotuo <youngtuotuo@protonmail.com>
2023-12-19 00:01:29 +0800

    fasodfupasd

--- nvim/lua/lazyload/aerial.lua
+++ nvim/lua/lazyload/aerial.lua
@@ -3,7 +3,7 @@ require("aerial").setup({
     default_direction = "left",
     max_width = 30,
     min_width = 30,
-  }
+  },
 })
 vim.keymap.set("n", "<space>o", function()
   require("aerial").toggle({ focus = false })
