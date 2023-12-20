22c85e8003c7bcbd3d9a68654b69a77497153b76
youngtuotuo <youngtuotuo@protonmail.com>
2023-12-20 13:24:41 +0800

    iofasdpfupsa

--- nvim/lua/lazyload/lsp/pyright.lua
+++ nvim/lua/lazyload/lsp/pyright.lua
@@ -1,41 +1,27 @@
 local g = require("tuo.global")
-local util = require('vim.lsp.util')
+local util = require("vim.lsp.util")
 
 local function split_lines(value)
-  value = string.gsub(value, '\r\n?',     '\n')
-  value = string.gsub(value, '&nbsp;',    ' ')
-  value = string.gsub(value, '\\',        '')
-  value = string.gsub(value, '```python', '')
-  value = string.gsub(value, '```',       '')
+  value = string.gsub(value, "\n%-%-%-", "%-%-%-")
+  value = string.gsub(value, "\\%[", "[[")
+  value = string.gsub(value, "\\%]", "]]")
+  value = string.gsub(value, "&nbsp;", " ")
+  value = string.gsub(value, "&gt;", ">")
+  value = string.gsub(value, "\\", "")
+  value = string.gsub(value, "```python", "")
+  value = string.gsub(value, "```", "")
   value = string.gsub(value, "`", "")
-  return vim.split(value, '\n', { plain = true, trimempty = true })
+  return vim.split(value, "\n", { plain = true, trimempty = true })
 end
 
 local function convert_input_to_markdown_lines(input, contents)
   contents = contents or {}
-  -- MarkedString variation 1
-  if type(input) == 'string' then
-    vim.list_extend(contents, split_lines(input))
-  else
-    assert(type(input) == 'table', 'Expected a table for LSP input')
-    -- MarkupContent
-    if input.kind then
-      local value = input.value or ''
-      vim.list_extend(contents, split_lines(value))
-      -- MarkupString variation 2
-    elseif input.language then
-      -- table.insert(contents, '```' .. input.language)
-      vim.list_extend(contents, split_lines(input.value or ''))
-      -- table.insert(contents, '```')
-      -- By deduction, this must be MarkedString[]
-    else
-      -- Use our existing logic to handle MarkedString
-      for _, marked_string in ipairs(input) do
-        convert_input_to_markdown_lines(marked_string, contents)
-      end
-    end
+  assert(type(input) == "table", "Expected a table for LSP input")
+  if input.kind then
+    local value = input.value or ""
+    vim.list_extend(contents, split_lines(value))
   end
-  if (contents[1] == '' or contents[1] == nil) and #contents == 1 then
+  if (contents[1] == "" or contents[1] == nil) and #contents == 1 then
     return {}
   end
   return contents
@@ -50,7 +36,7 @@ local function hover(_, result, ctx, config)
   end
   if not (result and result.contents) then
     if config.silent ~= true then
-      vim.notify('No information available')
+      vim.notify("No information available")
     end
     return
   end
@@ -58,13 +44,12 @@ local function hover(_, result, ctx, config)
   contents = convert_input_to_markdown_lines(result.contents)
   if vim.tbl_isempty(contents) then
     if config.silent ~= true then
-      vim.notify('No information available')
+      vim.notify("No information available")
     end
     return
   end
-  return util.open_floating_preview(contents, 'plaintext', config)
+  return util.open_floating_preview(contents, "markdown", config)
 end
-
 
 local config = function(capabilities, util)
   return {
@@ -81,7 +66,7 @@ local config = function(capabilities, util)
         { border = g.border, title = " Pyright ", max_width = 100 }
       ),
     },
-    capabilities =  capabilities,
+    capabilities = capabilities,
     root_dir = util.root_pattern(unpack({
       ".gitignore",
       "pyproject.toml",
