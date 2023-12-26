75cc10d3f7e6032ad3387fab2003bd97a51fcbbe
youngtuotuo <youngtuotuo@protonmail.com>
2023-12-26 23:03:36 +0800

    fasodfupasd

--- nvim/lua/lazyload/formatters/clang-format.lua
+++ nvim/lua/lazyload/formatters/clang-format.lua
@@ -1,7 +1,7 @@
 return function()
   return {
     exe = "clang-format",
-    args = { '--style="{BasedOnStyle: llvm, ColumnLimit: 100, IndentWidth: 4, AccessModifierOffset: -2}"' },
+    args = { '--style="{BasedOnStyle: llvm, ColumnLimit: 100, IndentWidth: 4, AccessModifierOffset: -2, IndentCaseBlocks: true}"' },
     stdin = true,
   }
 end
