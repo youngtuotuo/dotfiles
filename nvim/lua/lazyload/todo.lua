5747a0f1cce1489bbd5132c9aad9e95cdf2a0a62
youngtuotuo <youngtuotuo@protonmail.com>
2023-12-29 13:59:07 +0800

    iofasdpfupsa

--- nvim/lua/lazyload/todo.lua
+++ nvim/lua/lazyload/todo.lua
@@ -36,9 +36,9 @@ require("todo-comments").setup({
     multiline = false, -- enable multine todo comments
     multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
     multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
-    before = "fg", -- "fg" or "bg" or empty
-    keyword = "wide_fg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-    after = "fg", -- "fg" or "bg" or empty
+    before = "", -- "fg" or "bg" or empty
+    keyword = "bg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+    after = "", -- "fg" or "bg" or empty
     pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
     comments_only = true, -- uses treesitter to match keywords in comments only
     max_line_len = 400, -- ignore lines longer than this
