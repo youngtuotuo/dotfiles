cfa6d2187ea4c9dd95caefa60f2718f0743cd6f5
youngtuotuo <youngtuotuo@protonmail.com>
2024-01-08 11:44:29 +0800

    AAAAAA

--- nvim/lua/plugins/zen_mode.lua
+++ nvim/lua/plugins/zen_mode.lua
@@ -26,42 +26,22 @@ return {
             -- list = false, -- disable whitespace characters
           },
         },
+        -- callback where you can add custom code when the Zen window opens
+        on_open = function(win)
+          vim.opt.fillchars = "stl: ,stlnc: ,fold: ,foldopen:,foldsep: ,foldclose:"
+        end,
+        -- callback where you can add custom code when the Zen window closes
+        on_close = function()
+          vim.opt.fillchars = "stl: ,stlnc: ,fold: ,foldopen:,foldsep: ,foldclose:"
+        end,
         plugins = {
-          -- disable some global vim options (vim.o...)
-          -- comment the lines to not apply the options
           options = {
-            enabled = true,
-            ruler = false, -- disables the ruler text in the cmd line area
-            showcmd = false, -- disables the command in the last line of the screen
             -- you may turn on/off statusline in zen mode by setting 'laststatus'
             -- statusline will be shown only if 'laststatus' == 3
-            laststatus = 0, -- turn off the statusline in zen mode
+            laststatus = 3, -- turn off the statusline in zen mode
           },
           twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
-          gitsigns = { enabled = false }, -- disables git signs
-          tmux = { enabled = false }, -- disables the tmux statusline
-          -- this will change the font size on kitty when in zen mode
-          -- to make this work, you need to set the following kitty options:
-          -- - allow_remote_control socket-only
-          -- - listen_on unix:/tmp/kitty
-          kitty = {
-            enabled = false,
-            font = "+4", -- font size increment
-          },
-          -- this will change the font size on alacritty when in zen mode
-          -- requires  Alacritty Version 0.10.0 or higher
-          -- uses `alacritty msg` subcommand to change font size
-          alacritty = {
-            enabled = false,
-            font = "14", -- font size
-          },
-          -- this will change the font size on wezterm when in zen mode
-          -- See alse also the Plugins/Wezterm section in this projects README
-          wezterm = {
-            enabled = false,
-            -- can be either an absolute font size or the number of incremental steps
-            font = "+4", -- (10% increase per step)
-          },
+          gitsigns = { enabled = true }, -- disables git signs
         },
       })
     end,
