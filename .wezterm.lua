e32bf930ce5f21d2d7d0d10d4e399cad4335aa14
youngtuotuo <youngtuotuo@protonmail.com>
2024-01-17 13:27:10 +0800

    fasodfupasd

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,6 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
-
 -- This table will hold the configuration.
 local config = {}
 
@@ -12,10 +11,10 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-if package.config:sub(1,1) == "\\" then
+if package.config:sub(1, 1) == "\\" then
   config.default_prog = { "pwsh.exe" }
 end
-config.color_scheme = 'iTerm2 Default'
+config.color_scheme = "iTerm2 Default"
 config.font = wezterm.font("CaskaydiaCove Nerd Font", { weight = "Regular", italic = false })
 config.adjust_window_size_when_changing_font_size = false
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
@@ -28,6 +27,7 @@ config.mouse_bindings = {
   },
 }
 config.audible_bell = "Disabled"
+config.default_cursor_style = "SteadyBlock"
 
 config.window_frame = {
   border_left_width = "0.2cell",
