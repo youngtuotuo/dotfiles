18932f1883ce817c83219d8bdc3955ba5a04ad77
youngtuotuo <youngtuotuo@protonmail.com>
2024-01-18 19:08:49 +0800

    AAAAAA

--- .bashrc
+++ .bashrc
@@ -125,12 +125,4 @@ fi
 
 export EDITOR=nvim
 
-# from https://github.com/bartekspitza/dotfiles/blob/master/shell/case_insensitive_completion.sh
-# If ~/.inputrc doesn't exist yet: First include the original /etc/inputrc
-# so it won't get overriden
-if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
-
-# Add shell-option to ~/.inputrc to enable case-insensitive tab completion
-echo 'set completion-ignore-case On' >> ~/.inputrc
-
 echo -ne "\e[2 q"
--- install_config.sh
+++ install_config.sh
@@ -43,3 +43,13 @@ fi
 if ask "============ Do you want to link fd to fdfind? ============"; then
 	ln -s $(which fdfind) ~/.local/bin/fd
 fi
+
+# from https://github.com/bartekspitza/dotfiles/blob/master/shell/case_insensitive_completion.sh
+# If ~/.inputrc doesn't exist yet: First include the original /etc/inputrc
+# so it won't get overriden
+if ask "============ Do you want to set case case-insensitive in bash? ============"; then
+	if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' >~/.inputrc; fi
+
+	# Add shell-option to ~/.inputrc to enable case-insensitive tab completion
+	echo 'set completion-ignore-case On' >>~/.inputrc
+fi
