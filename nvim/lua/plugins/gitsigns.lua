592389ce0d9fae3d1c7ff2a341268eea15f5d325
youngtuotuo <youngtuotuo@protonmail.com>
2024-01-23 15:20:02 +0800

    AAAAAA

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -7,7 +7,7 @@ return {
     signs = {
       add          = { text = "│" },
       change       = { text = "│" },
-      delete       = { text = "_" },
+      delete       = { text = "-" },
       topdelete    = { text = "‾" },
       changedelete = { text = "~" },
       untracked    = { text = "┆" },
