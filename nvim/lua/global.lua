51d49247e9c8c21767f4978f22d0ae5fe786d44c
youngtuotuo <youngtuotuo@protonmail.com>
2024-01-24 22:03:43 +0800

    fuopasd

--- nvim/lua/global.lua
+++ nvim/lua/global.lua
@@ -36,7 +36,6 @@ _G.colorset = function()
     FoldColumn         = { fg = dgrey3, bg = transparent },
     WinBar             = { bg = transparent },
     WinBarNC           = { bg = transparent },
-    WhiteSpace         = { bg = r },
     netrwMarkFile      = { fg = y },
     markdownBlockquote = { fg = grey },
     Pmenu              = { bg = dgrey3 },
