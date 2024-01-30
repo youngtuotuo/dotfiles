b24a9a472312c7cef8b8c26accc1f92b6f2c0b5b
youngtuotuo <youngtuotuo@protonmail.com>
2024-01-30 15:44:39 +0800

    custom signature

--- nvim/lua/language_servers/handlers.lua
+++ nvim/lua/language_servers/handlers.lua
@@ -18,7 +18,7 @@ local function split_lines(value)
   if vim.fn.has("win32") == 0 then
     value = string.gsub(value, "\\", "")
   end
-  return { value }
+  return value
 end
 
 local function hover(_, result, ctx, config)
@@ -35,24 +35,67 @@ local function hover(_, result, ctx, config)
     return
   end
   return require("vim.lsp.util").open_floating_preview(
-    split_lines(result.contents.value),
+    { split_lines(result.contents.value) },
     -- result.contents.kind,
     "plaintext",
     config
   )
 end
 
+local function signature_help(_, result, ctx, config)
+  config = config or {}
+  config.focus_id = ctx.method
+  if vim.api.nvim_get_current_buf() ~= ctx.bufnr then
+    -- Ignore result since buffer changed. This happens for slow language servers.
+    return
+  end
+  -- When use `autocmd CompleteDone <silent><buffer> lua vim.lsp.buf.signature_help()` to call signatureHelp handler
+  -- If the completion item doesn't have signatures It will make noise. Change to use `print` that can use `<silent>` to ignore
+  if not (result and result.signatures and result.signatures[1]) then
+    if config.silent ~= true then
+      print('No signature help available')
+    end
+    return
+  end
+  result.signatures[1].documentation.value = split_lines(result.signatures[1].documentation.value)
+  local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+  local triggers = vim.tbl_get(client.server_capabilities, 'signatureHelpProvider', 'triggerCharacters')
+  local ft = vim.bo[ctx.bufnr].filetype
+  local lines, hl = require("vim.lsp.util").convert_signature_help_to_markdown_lines(result, ft, triggers)
+  if not lines or vim.tbl_isempty(lines) then
+    if config.silent ~= true then
+      print('No signature help available')
+    end
+    return
+  end
+  table.remove(lines, #lines)
+  if vim.startswith(lines[1], '```') then table.remove(lines, 1) end
+  if vim.startswith(lines[2], '```') then lines[2] = "---" end
+  local fbuf, fwin = require("vim.lsp.util").open_floating_preview(lines, 'plaintext', config)
+  if hl then
+    -- Highlight the second line if the signature is wrapped in a Markdown code block.
+    vim.api.nvim_buf_add_highlight(fbuf, -1, 'LspSignatureActiveParameter', 0, unpack(hl))
+  end
+  return fbuf, fwin
+end
+
 -- customize hover when pressing K
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(hover, {
-  border = _G.border,
-  title = " Hover ",
-  max_width = 100,
-  max_height = 20,
-  zindex = 500,
-  focusable = true,
-})
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  hover,
+  {
+    border = _G.border,
+    title = " |･ω･) ? ",
+    max_height = 20,
+    zindex = 500,
+    focusable = true,
+  }
+)
 -- customize signature help when pressing gs
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-  vim.lsp.handlers.signature_help,
-  { border = _G.border, title = " Signature ", max_width = 100 }
+  signature_help,
+  {
+    border = _G.border,
+    title = " (・・ ) ? ",
+    max_height = 20,
+  }
 )
