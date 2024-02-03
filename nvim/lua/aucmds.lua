573eb1a0313db6f19cb3ab87f636171e44aa92e6
youngtuotuo <youngtuotuo@protonmail.com>
2024-02-03 20:17:55 +0800

    cl in windows

--- nvim/lua/aucmds.lua
+++ nvim/lua/aucmds.lua
@@ -1,4 +1,3 @@
--- stylua: ignore start
 local check_math_h = function()
   local content = vim.api.nvim_buf_get_lines(0, 0, -1, false)
   if vim.fn.match(content, "math.h") == -1 then
@@ -10,33 +9,77 @@ end
 local group = vim.api.nvim_create_augroup("TuoGroup", { clear = true })
 
 local cmds = {
-  TextYankPost = { { callback = function() vim.highlight.on_yank() end, desc = "Yank Short Indicator" } },
-  TermOpen     = { { callback = function() vim.api.nvim_input("i") end, desc = "Enter Terminal with Insert Mode" } },
-  BufEnter     = {
+  TextYankPost = { {
+    callback = function()
+      vim.highlight.on_yank()
+    end,
+    desc = "Yank Short Indicator",
+  } },
+  TermOpen = { {
+    callback = function()
+      vim.api.nvim_input("i")
+    end,
+    desc = "Enter Terminal with Insert Mode",
+  } },
+  BufEnter = {
     {
-      pattern = { "*.c", "*.cpp" },
+      pattern = { "*.c" },
       callback = function()
         local bufname = vim.fn.expand("%:t:r")
-        local bufext = vim.fn.expand("%:e")
 
-        local compiler = bufext == "c" and "gcc" or "g++"
-        if vim.fn.has("win32") == 1 then
+        local compiler = "gcc" -- linux
+        if vim.fn.has("win32") == 1 then -- idiot os
           compiler = "cl"
-        elseif vim.fn.has("mac") == 1 then
+        elseif vim.fn.has("mac") == 1 then -- mac
           compiler = "clang"
         end
 
-        local cmd = string.format("%s -Wall -Wextra", compiler)
-        -- # include <math.h>
-        if check_math_h() then cmd = string.format("%s -lm", cmd) end
-        --  cpp17
-        if bufext == "cpp" then cmd = string.format("%s -std=c++17", cmd) end
-        cmd = string.format("%s -o %s%s %% && ./%s%s", cmd, bufname, _G.ext, bufname, _G.ext)
+        -- warning
+        local cmd = compiler
+        if vim.fn.has("win32") == 0 then -- fk u
+          cmd = string.format("%s -Wall -Wextra", cmd)
+          -- # include <math.h>
+          if check_math_h() then cmd = string.format("%s -lm", cmd) end
+          -- output 
+          cmd = string.format("%s -o %s %%", cmd, bufname)
+        elseif vim.fn.has("win32") == 1 then
+          cmd = string.format("%s -Zi %%", cmd)
+        end
+        -- execute
+        cmd = string.format("%s && %s%s", cmd, bufname, _G.ext)
         cmd = ":terminal " .. cmd .. " <C-b>"
         vim.keymap.set("n", "<leader>p", cmd)
         vim.keymap.set("v", "<leader>p", "<nop>")
       end,
-      desc = "<leader>p for c/c++"
+      desc = "<leader>p for c",
+    },
+    {
+      pattern = { "*.cpp" },
+      callback = function()
+        local bufname = vim.fn.expand("%:t:r")
+
+        local compiler = "g++" -- linux
+        if vim.fn.has("win32") == 1 then -- idiot os
+          compiler = "cl"
+        elseif vim.fn.has("mac") == 1 then -- mac
+          compiler = "clang++"
+        end
+
+        local cmd = compiler
+        if vim.fn.has("win32") == 0 then
+          cmd = string.format("%s -Wall -Wextra -std=c++17", compiler)
+          -- # include <math.h>
+          if check_math_h() then cmd = string.format("%s -lm", cmd) end
+          cmd = string.format("%s -o %s %%", cmd, bufname)
+        elseif vim.fn.has("win32") == 1 then
+          cmd = string.format("%s -Zi %%", cmd)
+        end
+        cmd = string.format("%s && %s%s", cmd, bufname, _G.ext)
+        cmd = ":terminal " .. cmd .. " <C-b>"
+        vim.keymap.set("n", "<leader>p", cmd)
+        vim.keymap.set("v", "<leader>p", "<nop>")
+      end,
+      desc = "<leader>p for c++",
     },
     {
       pattern = { "*.lua" },
@@ -46,7 +89,7 @@ local cmds = {
         vim.keymap.set("n", "<leader>p", ":terminal lua % <C-b>")
         vim.keymap.set("v", "<leader>p", ":w !lua")
       end,
-      desc = "<leader>p for lua"
+      desc = "<leader>p for lua",
     },
     {
       pattern = { "*.py" },
@@ -54,7 +97,7 @@ local cmds = {
         vim.keymap.set("n", "<leader>p", ":terminal python3 % <C-b>")
         vim.keymap.set("v", "<leader>p", ":w !python3")
       end,
-      desc = "<leader>p for python"
+      desc = "<leader>p for python",
     },
     {
       pattern = { "Makefile" },
@@ -63,7 +106,7 @@ local cmds = {
         vim.opt_local.softtabstop = 0
         vim.opt_local.expandtab = false
       end,
-      desc = "Change indent to tab for Makefile"
+      desc = "Change indent to tab for Makefile",
     },
     {
       pattern = { "*.mojo" },
@@ -75,7 +118,7 @@ local cmds = {
           vim.keymap.set("v", "<leader>p", ":w !mojo")
         end
       end,
-      desc = "<leader>p for mojo"
+      desc = "<leader>p for mojo",
     },
     {
       pattern = { "*.json", "*.html", "*.yaml" },
@@ -83,7 +126,7 @@ local cmds = {
         vim.opt_local.shiftwidth = 2
         vim.opt_local.softtabstop = 2
       end,
-      desc = "Change indent level to 2 for json, html, yaml"
+      desc = "Change indent level to 2 for json, html, yaml",
     },
     {
       pattern = { "*.tex" },
@@ -92,7 +135,7 @@ local cmds = {
         vim.keymap.set("n", "<leader>p", ":VimtexCompile")
         vim.keymap.set("v", "<leader>p", "<nop>")
       end,
-      desc = "<leader>p for latex"
+      desc = "<leader>p for latex",
     },
     {
       pattern = { "*.rs" },
@@ -100,7 +143,7 @@ local cmds = {
         vim.keymap.set("n", "<leader>p", ":terminal cargo run % <C-b>")
         vim.keymap.set("v", "<leader>p", "<nop>")
       end,
-      desc = "<leader>p for rust"
+      desc = "<leader>p for rust",
     },
     {
       pattern = { "*.go" },
@@ -111,7 +154,7 @@ local cmds = {
         vim.keymap.set("n", "<leader>p", ":terminal go run % <C-b>")
         vim.keymap.set("v", "<leader>p", "<nop>")
       end,
-      desc = "<leader>p for go"
+      desc = "<leader>p for go",
     },
     {
       pattern = { "*.zig" },
@@ -120,31 +163,31 @@ local cmds = {
         vim.keymap.set("n", "<leader>p", ":terminal zig run % <C-b>")
         vim.keymap.set("v", "<leader>p", "<nop>")
       end,
-      desc = "<leader>p for zig"
-    }
+      desc = "<leader>p for zig",
+    },
   },
   BufWinEnter = {
     {
       pattern = { "*.txt", "*.md" },
       callback = function()
         if vim.o.filetype == "help" or vim.o.filetype == "markdown" then
-          vim.cmd [[wincmd L]]
+          vim.cmd([[wincmd L]])
         end
       end,
-      desc = "Let help file go to vertical split"
+      desc = "Let help file go to vertical split",
     },
     {
       callback = function()
         vim.opt.formatoptions = "jql"
       end,
-      desc = "All buffer need formatoptions = jql"
-    }
+      desc = "All buffer need formatoptions = jql",
+    },
   },
   ColorScheme = {
     {
       callback = _G.colorset,
-      desc = "Auto set some colors for colorsecheme change"
-    }
+      desc = "Auto set some colors for colorsecheme change",
+    },
   },
 }
 
