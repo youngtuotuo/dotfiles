4b65b8a2740dc194df52d7cac73e0e9ebc2f8122
youngtuotuo <youngtuotuo@protonmail.com>
2024-02-12 19:21:17 +0800

    wsl usage as powershell function

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -20,4 +20,34 @@ function dev($name) {
     C:\'Program Files\Microsoft Visual Studio'\2022\Community\Common7\Tools\Launch-VsDevShell.ps1 -Arch $name;
     cd $curDir
 }
+
+function wsl_usage($name) {
+    Write-Output "############### WSL Disk Usage ###############"
+    Write-Output ""
+    Get-ChildItem "HKCU:\Software\Microsoft\Windows\CurrentVersion\Lxss" -Recurse |
+    ForEach-Object {
+        if (($_ | Split-Path -Leaf) -ne "TryStoreWSL") {
+            $distro_name = ($_ | Get-ItemProperty -Name DistributionName).DistributionName
+            $distro_dir =  ($_ | Get-ItemProperty -Name BasePath).BasePath
+            $distro_dir = Switch ($PSVersionTable.PSEdition) {
+              "Core" {
+                $distro_dir -replace '^\\\\\?\\',''
+              }
+              "Desktop" {
+                if ($distro_dir.StartsWith('\\?\')) {
+                    $distro_dir
+                } else {
+                    '\\?\' + $distro_dir
+                }
+              }
+            }
+            Write-Output "Distribution: $distro_name"
+            Write-Output "Directory: $($distro_dir -replace '\\\\\?\\','')"
+            $distro_size = "{0:N0} MB" -f ((Get-ChildItem -Recurse -LiteralPath "$distro_dir" | Measure-Object -Property Length -sum).sum / 1Mb)
+            Write-Output "Size: $distro_size"
+            Write-Output "------------------------------"
+        }
+    }
+}
+
 oh-my-posh init pwsh --config 'C:/Users/User/AppData/Local/Programs/oh-my-posh/themes/robbyrussell.omp.json' | Invoke-Expression
