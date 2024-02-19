04e11c8d153e4cd9d37407eb485f5b5b25338317
youngtuotuo <youngtuotuo@protonmail.com>
2024-02-19 19:36:12 +0800

    frequently used snippet print with f string in python

--- nvim/lua/snippets/python.lua
+++ nvim/lua/snippets/python.lua
@@ -1,1 +1,20 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local fmta = require("luasnip.extras.fmt").fmta
+
+local snippets = {}
+
+snippets = vim.tbl_extend("force", snippets, {
+  s({ trig = "pff", snippetType = "autosnippet" },
+    fmta(
+      [[
+      print(f"{<>=}")
+      ]],
+      { i(1) }
+    )
+  )
+})
+
+return snippets
 
