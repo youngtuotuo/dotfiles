bcd8510bb81593efa49c6efcb05c4ce9b3064620
youngtuotuo <youngtuotuo@protonmail.com>
2024-02-21 12:17:17 +0800

    fix yarn install scripts

--- ubuntu/sudo_install.sh
+++ ubuntu/sudo_install.sh
@@ -51,7 +51,8 @@ if ask "============ Do you want to install yarn? ============"; then
 		curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 		echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 		sudo apt-get update && sudo apt-get install yarn
-		echo -e "\033[93mINFO\033[0m yarn exists: $(which yarn)"
+	else
+        echo -e "\033[93mINFO\033[0m yarn exists: $(which yarn)"
 	fi
 fi
 
