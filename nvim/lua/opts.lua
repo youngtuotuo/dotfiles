1e5864874f09f65949a87a386a9cd8e63995e250
youngtuotuo <youngtuotuo@protonmail.com>
2024-03-11 17:37:12 +0800

    shada with % not useful

--- nvim/lua/opts.lua
+++ nvim/lua/opts.lua
@@ -36,7 +36,7 @@ vim.opt.formatoptions = "jql" -- :h fo-table
 vim.opt.termguicolors = true
 vim.opt.grepprg = [[grep -rn $*]]
 vim.opt.path = [[.,,**]]
-vim.opt.shada = [[!,'100,<50,s10,h,%]]
+vim.opt.shada = [[!,'100,<50,s10,h]]
 
 -- append options
 vim.opt.wildignore:append({ "*.o", "*~", "*.pyc", "*pycache*" })
