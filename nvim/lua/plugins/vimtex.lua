0b7eead3f59a7a1ac893b8c2190153b3766cc6f0
youngtuotuo <youngtuotuo@protonmail.com>
2024-03-28 17:01:26 +0800

    solve vimtex mapping

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -5,5 +5,6 @@ return {
     vim.g.vimtex_view_method = "sioyek"
     vim.g.vimtex_quickfix_enabled = 0
     vim.g.vimtex_imaps_enabled = 0
+    vim.keymap.set({ "i" }, "<C-x><cr>", "<plug>(vimtex-delim-close)")
   end,
 }
