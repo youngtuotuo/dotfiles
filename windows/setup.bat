17dc139afa1a32536710530e9038732361b7fbe4
youngtuotuo <youngtuotuo@protonmail.com>
2024-03-28 19:02:52 +0800

    windows latex

--- windows/setup.bat
+++ windows/setup.bat
@@ -11,7 +11,8 @@ if "!response!"=="" (
 set response=!response:~0,1!
 if "!response!"=="Y" goto clink_install
 if "!response!"=="y" goto clink_install
-goto alias
+goto end
+
 
 :clink_install
 scoop install clink
@@ -20,6 +21,22 @@ cp %USERPROFILE%\github\dotfiles\windows\clink_start.cmd %=clink.profile%
 clink installscripts %USERPROFILE%\github\dotfiles\windows\clink_scripts
 clink set tilde.autoexpand true
 clink set autosuggest.enable false
+goto latex
+
+:latex
+echo.============ Do you want to install latex? ========== (Y/n):
+set /p response=
+if "!response!"=="" (
+  set response=n
+)
+set response=!response:~0,1!
+if "!response!"=="Y" goto latex_install
+if "!response!"=="y" goto latex_install
+goto end
+
+:latex_install
+scoop install latex
+echo Please use miktex console to install latexmk
 goto end
 
 :end
