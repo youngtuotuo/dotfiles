8a276c28d7c94f31ec3d4bf71f38f5abe547a472
youngtuotuo <youngtuotuo@protonmail.com>
2024-04-02 15:28:06 +0800

    keep linting is really annoying

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -8,7 +8,7 @@ return {
     require("lint").linters_by_ft = {
       python = { "ruff" },
     }
-    vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "BufWritePost", "TextChanged" }, {
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
       group = _G.auG,
       callback = function()
         require("lint").try_lint()
