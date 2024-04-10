a172aedcfedabbc233997f2e2e5d9ddd7980a4d4
youngtuotuo <youngtuotuo@protonmail.com>
2024-04-10 11:15:57 +0800

    back pwsh

--- nvim/lua/global.lua
+++ nvim/lua/global.lua
@@ -22,6 +22,15 @@ _G.lspfts = { "zig" }
 _G.sep = vim.fn.has("win32") == 1 and [[\]] or "/"
 _G.home = vim.fn.has("win32") == 1 and "USERPROFILE" or "HOME"
 _G.ext = vim.fn.has("win32") == 1 and ".exe" or ""
+if vim.fn.has("win32") == 1 then
+  vim.cmd([[
+    let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+    let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+    let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+    let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+    set shellquote= shellxquote=
+  ]])
+end
 
 _G.auG = "TuoGroup"
 
