805b5ba969cc714875e2490d2b1028c9a9e38c6c
youngtuotuo <youngtuotuo@protonmail.com>
2024-04-24 14:15:42 +0800

    neovide

--- nvim/lua/opts.lua
+++ nvim/lua/opts.lua
@@ -115,3 +115,8 @@ if vim.fn.has("win32") == 1 then
   ]])
 end
 
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0.01
+  vim.g.neovide_cursor_trail_size = 0.01
+  vim.g.neovide_cursor_animate_command_line = false
+end
