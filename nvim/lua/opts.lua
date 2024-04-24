5742a711910719710738e3e5ff6b9e15af00d1d6
youngtuotuo <youngtuotuo@protonmail.com>
2024-04-24 14:24:08 +0800

    any nvim gui waste time

--- nvim/lua/opts.lua
+++ nvim/lua/opts.lua
@@ -114,9 +114,3 @@ if vim.fn.has("win32") == 1 then
     set shellquote= shellxquote=
   ]])
 end
-
-if vim.g.neovide then
-  vim.g.neovide_cursor_animation_length = 0.01
-  vim.g.neovide_cursor_trail_size = 0.01
-  vim.g.neovide_cursor_animate_command_line = false
-end
