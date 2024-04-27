861784f4fbefa2a47f89f685e651e65682bbe948
youngtuotuo <youngtuotuo@protonmail.com>
2024-04-27 18:19:57 +0800

    gruber

--- nvim/lua/plugins/gruber-darker-nvim.lua
+++ nvim/lua/plugins/gruber-darker-nvim.lua
@@ -12,7 +12,7 @@ return {
     vim.cmd.colo "gruber-darker"
     _G.colorset()
     vim.api.nvim_set_hl(0, "Cursor", { fg = "#000000", bg = "#cccccc" })
-    vim.api.nvim_set_hl(0, "Comment", { fg = "DarkGrey", italic = true })
+    -- vim.api.nvim_set_hl(0, "Comment", { fg = "DarkGrey", italic = true })
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
   end
