11d5d22b238c89dcfcb8440aacc1c2aedaf42403
youngtuotuo <youngtuotuo@protonmail.com>
2024-04-29 19:57:58 +0800

    catpputtin

---
+++ nvim/lua/plugins/catppuccin-nvim.lua
@@ -0,0 +1,23 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    transparent_background = true,
+    show_end_of_buffer = true,
+    term_colors = false,
+    dim_inactive = { enabled = false },
+    no_italic = true,
+    no_bold = true,
+    no_underline = true,
+    default_integrations = false,
+    integrations = {
+      aerial = true
+    }
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colo [[catppuccin]]
+    _G.colorset()
+  end
+}
--- nvim/lua/plugins/gruber-darker-nvim.lua
+++ nvim/lua/plugins/gruber-darker-nvim.lua
@@ -1,5 +1,6 @@
 return {
   "blazkowolf/gruber-darker.nvim",
+  lazy = true,
   opts = {
     italic = {
       strings = false,
@@ -9,11 +10,11 @@ return {
   },
   config = function(_, opts)
     require("gruber-darker").setup(opts)
-    vim.cmd.colo "gruber-darker"
-    _G.colorset()
+    -- vim.cmd.colo "gruber-darker"
+    -- _G.colorset()
     -- vim.api.nvim_set_hl(0, "Cursor", { fg = "#000000", bg = "#cccccc" })
     -- vim.api.nvim_set_hl(0, "Comment", { fg = "DarkGrey", italic = true })
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
   end
 }
