1a468a82c75ca3674151da9cf7bc1a2eb2d04f9a
youngtuotuo <youngtuotuo@protonmail.com>
2024-05-08 17:48:52 +0800

    bad pyright

--- nvim/lua/plugins/mason-nvim.lua
+++ nvim/lua/plugins/mason-nvim.lua
@@ -45,6 +45,52 @@ function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   opts.wrap = _G.floatwrap
   return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
+local util = require("vim.lsp.util")
+
+local function split_lines(value)
+  value = string.gsub(value, "&nbsp;", " ")
+  value = string.gsub(value, "&gt;", ">")
+  value = string.gsub(value, "&lt;", "<")
+  value = string.gsub(value, "\\", "")
+  return vim.split(value, "\n", { plain = true, trimempty = true })
+end
+
+local function convert_input_to_markdown_lines(input, contents)
+  contents = contents or {}
+  assert(type(input) == "table", "Expected a table for LSP input")
+  if input.kind then
+    local value = input.value or ""
+    vim.list_extend(contents, split_lines(value))
+  end
+  if (contents[1] == "" or contents[1] == nil) and #contents == 1 then
+    return {}
+  end
+  return contents
+end
+
+local function hover(_, result, ctx, config)
+  config = config or {}
+  config.focus_id = ctx.method
+  if vim.api.nvim_get_current_buf() ~= ctx.bufnr then
+    -- Ignore result since buffer changed. This happens for slow language servers.
+    return
+  end
+  if not (result and result.contents) then
+    if config.silent ~= true then
+      vim.notify("No information available")
+    end
+    return
+  end
+  local contents ---@type string[]
+  contents = convert_input_to_markdown_lines(result.contents)
+  if vim.tbl_isempty(contents) then
+    if config.silent ~= true then
+      vim.notify("No information available")
+    end
+    return
+  end
+  return util.open_floating_preview(contents, "markdown", config)
+end
 
 return {
   {
@@ -123,6 +169,19 @@ return {
         },
       })
       require("lspconfig").pyright.setup({
+        handlers = {
+          ["textDocument/publishDiagnostics"] = function() end,
+          ["textDocument/hover"] = vim.lsp.with(hover, {
+            border = _G.border,
+            title = " Pyright ",
+            max_width = 100,
+            zindex = 500,
+          }),
+          ["textDocument/signatureHelp"] = vim.lsp.with(
+            vim.lsp.handlers.signature_help,
+            { border = _G.border, title = " Pyright ", max_width = 100 }
+          ),
+        },
         settings = {
           pyright = {
             disableOrganizeImports = false,
