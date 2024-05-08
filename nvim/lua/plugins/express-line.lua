03c1b95504d89496fb11f291aa075b926c781349
youngtuotuo <youngtuotuo@protonmail.com>
2024-05-08 23:04:59 +0800

    express line

--- nvim/lua/plugins/express-line.lua
+++ nvim/lua/plugins/express-line.lua
@@ -22,78 +22,9 @@ return {
       return s
     end
 
-    local git_changes_formatter = function(s)
-      local specs = {
-        insert = {
-          regex = "%+(%d+)",
-          icon = "+",
-          hl = "diffAdded",
-        },
-        change = {
-          regex = "%~(%d+)",
-          icon = "~",
-          hl = "diffChanged",
-        },
-        delete = {
-          regex = "%-(%d+)",
-          icon = "-",
-          hl = "diffRemoved",
-        },
-      }
-      local result = {}
-      for _, v in pairs(specs) do
-        local count = nil
-        count = tonumber(string.match(s, v.regex))
-        if count and count > 0 then
-          table.insert(result, set_hl(v.hl, ("%s%d"):format(v.icon, count)))
-        end
-      end
-      return table.concat(result, " ")
-    end
-
-    local git = subscribe.buf_autocmd("el_git_branch", "BufEnter", function(window, buffer)
-      local branch = extensions.git_branch(window, buffer)
-      local changes = extensions.git_changes(window, buffer)
-      local res = ""
-      if branch then
-        res = res .. set_hl("@constant", "  " .. branch)
-      end
-      if changes then
-        changes = git_changes_formatter(changes)
-        res = res .. " " .. changes
-      end
-      return res
-    end)
-
-    local function diag_formatter(opts)
-      return function(_, _, counts)
-        local items = {}
-        local icons = {
-          ["errors"] = { opts.icon_err or "E", opts.hl_err },
-          ["warnings"] = { opts.icon_warn or "W", opts.hl_warn },
-          ["infos"] = { opts.icon_info or "I", opts.hl_info },
-          ["hints"] = { opts.icon_hint or "H", opts.hl_hint },
-        }
-        for _, k in ipairs({ "errors", "warnings", "infos", "hints" }) do
-          if counts[k] > 0 then
-            table.insert(items, set_hl(icons[k][2], ("%s:%s"):format(icons[k][1], counts[k])))
-          end
-        end
-        local fmt = opts.fmt or "%s"
-        if vim.tbl_isempty(items) then
-          return ""
-        else
-          if not vim.tbl_isempty(items) then
-            local contents = ("%s"):format(table.concat(items, " "))
-            return fmt:format(contents)
-          end
-        end
-      end
-    end
-
-    local get_buffer_counts = function(diagnostic, _, buffer)
+    local function diagnostics(_, buffer)
       local counts = { 0, 0, 0, 0 }
-      local diags = diagnostic.get(buffer.bufnr)
+      local diags = vim.diagnostic.get(buffer.bufnr)
       if diags and not vim.tbl_isempty(diags) then
         for _, d in ipairs(diags) do
           if tonumber(d.severity) then
@@ -101,97 +32,123 @@ return {
           end
         end
       end
-      return {
+      counts = {
         errors = counts[1],
         warnings = counts[2],
         infos = counts[3],
         hints = counts[4],
       }
-    end
-
-    local function wrap_fnc(opts, fn)
-      return function(window, buffer)
-        -- buf_autocmd doesn't send win
-        if not window and buffer then
-          window = { win_id = vim.fn.bufwinid(buffer.bufnr) }
+      local items = {}
+      local icons = {
+        ["errors"] = { "E", "DiagnosticError" },
+        ["warnings"] = { "W", "DiagnosticWarn" },
+        ["infos"] = { "I", "DiagnosticInfo" },
+        ["hints"] = { "H", "DiagnosticHint" },
+      }
+      for _, k in ipairs({ "errors", "warnings", "infos", "hints" }) do
+        if counts[k] > 0 then
+          table.insert(items, set_hl(icons[k][2], ("%s%s"):format(icons[k][1], counts[k])))
         end
-        if opts.hide_inactive and window and window.win_id ~= vim.api.nvim_get_current_win() then
-          return ""
-        end
-        return fn(window, buffer)
       end
+      local fmt = " %s"
+      if vim.tbl_isempty(items) then
+        return ""
+      end
+      local contents = ("%s"):format(table.concat(items, " "))
+      return fmt:format(contents)
     end
 
-    local diagnostics = function(opts)
-      opts = opts or {}
-      local formatter = opts.formatter or diag_formatter(opts)
-      return subscribe.buf_autocmd(
-        "el_buf_diagnostic",
-        "DiagnosticChanged",
-        wrap_fnc(opts, function(window, buffer)
-          return formatter(window, buffer, get_buffer_counts(vim.diagnostic, window, buffer))
-        end)
-      )
+    local function git(window, buffer)
+      local branch = extensions.git_branch(window, buffer)
+      local changes = extensions.git_changes(window, buffer)
+      local res = ""
+      if branch then
+        res = res .. set_hl("@constant", " " .. branch)
+      end
+      if changes then
+        local specs = {
+          insert = {
+            regex = "%+(%d+)",
+            icon = "+",
+            hl = "diffAdded",
+          },
+          change = {
+            regex = "%~(%d+)",
+            icon = "~",
+            hl = "diffChanged",
+          },
+          delete = {
+            regex = "%-(%d+)",
+            icon = "-",
+            hl = "diffRemoved",
+          },
+        }
+        local result = {}
+        for _, v in pairs(specs) do
+          local count = nil
+          count = tonumber(string.match(changes, v.regex))
+          if count and count > 0 then
+            table.insert(result, set_hl(v.hl, ("%s%d"):format(v.icon, count)))
+          end
+        end
+        res = res .. " " .. table.concat(result, " ")
+      end
+      return res
     end
 
-    local modes = {
-      n = { "Normal", "N", { "@function" } },
-      niI = { "Normal", "N", { "@function" } },
-      niR = { "Normal", "N", { "@function" } },
-      niV = { "Normal", "N", { "@functio" } },
-      no = { "N·OpPd", "?", { "@function" } },
-      v = { "Visual", "V", { "Directory" } },
-      V = { "V·Line", "Vl", { "Directory" } },
-      [""] = { "V·Blck", "Vb", { "Directory" } },
-      s = { "Select", "S", { "Search" } },
-      S = { "S·Line", "Sl", { "Search" } },
-      [""] = { "S·Block", "Sb", { "Search" } },
-      i = { "Insert", "I", { "@constant" } },
-      ic = { "ICompl", "Ic" },
-      R = { "Rplace", "R", { "WarningMsg", "IncSearch" } },
-      Rv = { "VRplce", "Rv", { "WarningMsg", "IncSearch" } },
-      c = { "Cmmand", "C", { "diffAdded", "DiffAdd" } },
-      cv = { "Vim Ex", "E" },
-      ce = { "Ex (r)", "E" },
-      r = { "Prompt", "P" },
-      rm = { "More  ", "M" },
-      ["r?"] = { "Cnfirm", "Cn" },
-      ["!"] = { "Shell ", "S", { "DiffAdd", "diffAdded" } },
-      nt = { "Term  ", "T", { "Visual" } },
-      t = { "Term  ", "T", { "DiffAdd", "diffAdded" } },
-    }
-
-    local mode = function(opts)
-      opts = opts or {}
-      return wrap_fnc(opts, function(_, _)
-        local fmt = opts.fmt or "%s%s"
-        local mode = vim.api.nvim_get_mode().mode
-        local mode_data = opts.modes and opts.modes[mode]
-        local hls = mode_data and mode_data[3]
-        local icon = opts.hl_icon_only and set_hl(hls, opts.icon) or opts.icon
-        mode = mode_data and mode_data[1]:upper() or mode
-        mode = (fmt):format(icon or "", mode)
-        return not opts.hl_icon_only and set_hl(hls, mode) or mode
-      end)
+    local function mode(_, _)
+      local modes = {
+        n = { "Normal", { "@function" } },
+        niI = { "Normal", { "@function" } },
+        niR = { "Normal", { "@function" } },
+        niV = { "Normal", { "@functio" } },
+        no = { "N·OpPd", { "@function" } },
+        v = { "Visual", { "Directory" } },
+        V = { "V·Line", { "Directory" } },
+        [""] = { "V·Blck", { "Directory" } },
+        s = { "Select", { "Search" } },
+        S = { "S·Line", { "Search" } },
+        [""] = { "S·Block", { "Search" } },
+        i = { "Insert", { "@constant" } },
+        ic = { "ICompl" },
+        R = { "Rplace", { "WarningMsg", "IncSearch" } },
+        Rv = { "VRplce", { "WarningMsg", "IncSearch" } },
+        c = { "Cmmand", { "diffAdded", "DiffAdd" } },
+        cv = { "Vim Ex" },
+        ce = { "Ex (r)" },
+        r = { "Prompt" },
+        rm = { "More  " },
+        ["r?"] = { "Cnfirm" },
+        ["!"] = { "Shell ", { "DiffAdd", "diffAdded" } },
+        nt = { "Term  ", { "Visual" } },
+        t = { "Term  ", { "DiffAdd", "diffAdded" } },
+      }
+      local fmt = "%s "
+      local m = vim.api.nvim_get_mode().mode
+      local mode_data = modes and modes[m]
+      local hls = mode_data and mode_data[2]
+      m = mode_data and mode_data[1]:upper() or m
+      m = (fmt):format(m)
+      return set_hl(hls, m) or m
     end
 
     require("el").setup({
       generator = function(_, _)
         local items = {
-          { mode({ modes = modes, fmt = "%s %s ", icon = "", hl_icon_only = false }) },
+          -- mode
+          {
+            mode,
+          },
           { sections.maximum_width(builtin.file_relative, 0.60), required = true },
           { sections.collapse_builtin({ { " " }, { builtin.modified_flag } }) },
+          -- diagnostics
           {
-            diagnostics({
-              fmt = " %s",
-              hl_err = "DiagnosticError",
-              hl_warn = "DiagnosticWarn",
-              hl_info = "DiagnosticInfo",
-              hl_hint = "DiagnosticHint",
-            }),
+            subscribe.buf_autocmd("el_buf_diagnostic", "DiagnosticChanged", diagnostics),
           },
           { sections.split, required = true },
-          { git },
+          {
+            subscribe.buf_autocmd("el_git_branch", "BufEnter", git),
+          },
           { " " },
           {
             sections.collapse_builtin({
@@ -202,7 +159,7 @@ return {
             }),
           },
           { builtin.filetype },
-        }
+        },
 
         local result = {}
         for _, item in ipairs(items) do
