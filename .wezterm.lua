152b026f4a71753344e27840f965d4ec1ba5f24e
youngtuotuo <youngtuotuo@protonmail.com>
2024-06-30 14:11:22 +0800

    wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,11 +10,31 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+config.keys = {
+  { key = "l", mods = "ALT", action = wezterm.action.ShowLauncher },
+}
+
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   config.font = wezterm.font("Cascadia Mono", { weight = "Regular" })
   config.default_prog = { "pwsh.exe", "-nologo" }
+  table.insert(
+    config.keys,
+    { key = "t", mods = "CTRL|SHIFT",
+      action = wezterm.action.SpawnCommandInNewTab({
+        cwd = wezterm.home_dir
+      })
+    }
+  )
 elseif wezterm.target_triple == "aarch64-apple-darwin" then
   config.font = wezterm.font("SF Mono", { weight = "Regular" })
+  table.insert(
+    config.keys,
+    { key = "t", mods = "CMD",
+      action = wezterm.action.SpawnCommandInNewTab({
+        cwd = wezterm.home_dir
+      })
+    }
+  )
 elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
   config.font = wezterm.font("JetBrains Mono", { weight = "Regular" })
 end
@@ -34,10 +54,6 @@ config.ssh_domains = {
 }
 
 config.hide_tab_bar_if_only_one_tab = true
-
-config.keys = {
-  { key = "l", mods = "ALT", action = wezterm.action.ShowLauncher },
-}
 
 -- and finally, return the configuration to wezterm
 return config
