8aa7feed0fa9ae23366a8f5a92c22d981a800d37
youngtuotuo <youngtuotuo@protonmail.com>
2024-07-11 15:15:59 +0800

    vim-illuminate

--- nvim/lua/plugins/illuminate-nvim.lua
+++ nvim/lua/plugins/illuminate-nvim.lua
@@ -18,5 +18,16 @@ return {
   },
   config = function(_, opts)
     require("illuminate").configure(opts)
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { underline = false })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { underline = false })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { underline = false })
+    vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+      pattern = { "*" },
+      callback = function(ev)
+        vim.api.nvim_set_hl(0, "IlluminatedWordText", { underline = false })
+        vim.api.nvim_set_hl(0, "IlluminatedWordRead", { underline = false })
+        vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { underline = false })
+      end,
+    })
   end,
 }
