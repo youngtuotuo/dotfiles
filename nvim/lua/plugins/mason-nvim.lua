919448ef7adb88cb259ef4ce2afb562b21330e02
youngtuotuo <youngtuotuo@protonmail.com>
2024-07-13 14:59:54 +0800

    cmp

--- nvim/lua/plugins/mason-nvim.lua
+++ nvim/lua/plugins/mason-nvim.lua
@@ -160,8 +160,6 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          -- ["<C-y>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
           ["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
