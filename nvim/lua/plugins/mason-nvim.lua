7ceaad42c1028046e83094a7b433c6d427a507c1
youngtuotuo <youngtuotuo@protonmail.com>
2024-07-14 18:07:42 +0800

    lspkind

--- nvim/lua/plugins/mason-nvim.lua
+++ nvim/lua/plugins/mason-nvim.lua
@@ -142,10 +142,21 @@ return {
       "hrsh7th/cmp-buffer",
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
+      "onsails/lspkind.nvim",
     },
     config = function(_, _)
       local cmp = require("cmp")
       cmp.setup({
+        formatting = {
+          format = require("lspkind").cmp_format({
+            mode = "symbol", -- show only symbol annotations
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            -- can also be a function to dynamically calculate max width such as
+            -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+            ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+          }),
+        },
         snippet = {
           expand = function(args)
             require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
