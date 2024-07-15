52f3b14c0629297c443eb1bcb80f35b5ba6bb8c1
youngtuotuo <youngtuotuo@protonmail.com>
2024-07-15 13:46:00 +0800

    devicons

--- nvim/lua/plugins/gitsigns-nvim.lua
+++ nvim/lua/plugins/gitsigns-nvim.lua
@@ -1,6 +1,14 @@
 return {
   "lewis6991/gitsigns.nvim",
   opts = {
+    preview_config = {
+      -- Options passed to nvim_open_win
+      border = "rounded",
+      style = "minimal",
+      relative = "cursor",
+      row = 0,
+      col = 1,
+    },
     on_attach = function(bufnr)
       local gitsigns = require("gitsigns")
 
