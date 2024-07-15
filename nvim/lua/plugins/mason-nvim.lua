454e47cc6e3cb2853f0eb5a9bdfec0fcb1da917c
youngtuotuo <youngtuotuo@protonmail.com>
2024-07-15 15:55:22 +0800

    nvim-cmp

--- nvim/lua/plugins/mason-nvim.lua
+++ nvim/lua/plugins/mason-nvim.lua
@@ -149,12 +149,10 @@ return {
       cmp.setup({
         formatting = {
           format = require("lspkind").cmp_format({
-            mode = "symbol", -- show only symbol annotations
-            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-            -- can also be a function to dynamically calculate max width such as
-            -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+            mode = "symbol_text", -- show only symbol annotations
+            maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
             ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+            show_labelDetails = false, -- show labelDetails in menu. Disabled by default
           }),
         },
         snippet = {
