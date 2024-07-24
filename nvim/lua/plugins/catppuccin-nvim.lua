9f9e42092143af57ff6ab46ff3c981037d8bdefd
youngtuotuo <youngtuotuo@protonmail.com>
2024-07-24 22:01:29 +0800

    catppuccin

---
+++ nvim/lua/plugins/catppuccin-nvim.lua
@@ -0,0 +1,27 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    no_italic = true,
+    transparent_background = true,
+    tyles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+      comments = {},
+      conditionals = { "bold" },
+      loops = { "bold" },
+      functions = { "bold" },
+      keywords = { "bold" },
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = { "bold" },
+      properties = {},
+      types = { "bold" },
+      operators = {},
+    },
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+  end,
+}
