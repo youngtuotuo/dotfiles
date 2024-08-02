e2bef945e0ca3604a6d517fe0729b1d1ba35e5cf
youngtuotuo <youngtuotuo@protonmail.com>
2024-08-02 19:53:46 +0800

    customize tender

--- nvim/lua/plugins/tender-nvim.lua
+++ nvim/lua/plugins/tender-nvim.lua
@@ -2,11 +2,15 @@ return {
   "jacoborus/tender.vim",
   config = function()
     vim.cmd.colorscheme "tender"
-    vim.api.nvim_set_hl(0, "MatchParen",    { link = "Statement" })
-    vim.api.nvim_set_hl(0, "Operator",      { link = "Normal" })
+    vim.api.nvim_set_hl(0, "Statement",     { ctermfg = 81,  bold = true, nocombine = false })
+    vim.api.nvim_set_hl(0, "Boolean",       { ctermfg = 215, bold = true, nocombine = false })
+    vim.api.nvim_set_hl(0, "Function",      { ctermfg = 153, bold = true, nocombine = false })
+    vim.api.nvim_set_hl(0, "MatchParen",    { ctermfg = 81 })
+    vim.api.nvim_set_hl(0, "Operator",      { ctermfg = "NONE" })
     vim.api.nvim_set_hl(0, "Normal",        { ctermbg = "NONE" })
-    vim.api.nvim_set_hl(0, "luaParenError", { ctermfg = "NONE" })
+    vim.api.nvim_set_hl(0, "luaParenError", { link    = "Normal" })
     vim.api.nvim_set_hl(0, "ModeMsg",       { ctermfg = "NONE" })
     vim.api.nvim_set_hl(0, "SignColumn",    { ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, "String",        { ctermfg = 78 })
   end
 }
