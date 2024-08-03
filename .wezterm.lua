b57d4e496e08f16687c7445839c155341915276e
youngtuotuo <youngtuotuo@protonmail.com>
2024-08-03 13:15:53 +0800

    .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -60,7 +60,7 @@ config.window_padding = { left = 0, right = 15, top = 0, bottom = 0 }
 config.enable_scroll_bar = true
 
 config.adjust_window_size_when_changing_font_size = false
--- config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.selection_word_boundary = " \t\n{}[]()\"'`@.,;:"
 config.audible_bell = "Disabled"
 
