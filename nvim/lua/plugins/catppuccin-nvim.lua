09fc8a373eeea2303600551f8a905a4afc2a95a3
youngtuotuo <youngtuotuo@protonmail.com>
2024-08-09 22:08:57 +0800

    catppuccin

--- nvim/lua/plugins/catppuccin-nvim.lua
+++ nvim/lua/plugins/catppuccin-nvim.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     flavour = "frappe",
     no_italic = true,
-    transparent_background = false,
+    transparent_background = true,
     integrations = {
       treesitter = true,
     },
