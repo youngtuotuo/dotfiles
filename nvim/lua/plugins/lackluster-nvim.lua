9e99f56ab1f54268ee4dee928cf0790595751338
youngtuotuo <youngtuotuo@protonmail.com>
2024-08-20 14:18:05 +0800

    lackluster

---
+++ nvim/lua/plugins/lackluster-nvim.lua
@@ -0,0 +1,15 @@
+-- example lazy.nvim install setup
+return {
+  "slugbyte/lackluster.nvim",
+  lazy = false,
+  priority = 1000,
+  init = function()
+    vim.cmd.colorscheme("lackluster")
+    -- vim.cmd.colorscheme("lackluster-hack")
+    -- vim.cmd.colorscheme("lackluster-mint")
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#292929" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#292929" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#292929" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  end,
+}
--- nvim/lua/plugins/monochrome-nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "kdheepak/monochrome.nvim",
-  config = function()
-    vim.cmd.colorscheme "monochrome"
-    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#292929" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#292929" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#292929" })
-  end,
-}
