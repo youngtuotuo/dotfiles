6650beb5ccbb0cdee1e0391b35c516149119f32a
youngtuotuo <youngtuotuo@protonmail.com>
2024-08-20 14:22:41 +0800

    lackluster

--- nvim/lua/plugins/lackluster-nvim.lua
+++ nvim/lua/plugins/lackluster-nvim.lua
@@ -11,5 +11,7 @@ return {
     vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#292929" })
     vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#292929" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LazyNormal", { bg = "none" })
   end,
 }
