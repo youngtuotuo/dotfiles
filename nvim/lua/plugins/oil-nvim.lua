c3f1e37e87fd656f319eff3d84d9e3fac958c147
Mike Hung <mike@powerarena.com>
2024-08-22 11:48:15 +0800

    oil toggle

--- nvim/lua/plugins/oil-nvim.lua
+++ nvim/lua/plugins/oil-nvim.lua
@@ -1,4 +1,4 @@
-local detaul = true
+local detail = true
 return {
   "stevearc/oil.nvim",
   opts = {
@@ -8,6 +8,17 @@ return {
       "mtime",
     },
     keymaps = {
+      ["gd"] = {
+        desc = "Toggle file detail view",
+        callback = function()
+          detail = not detail
+          if detail then
+            require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+          else
+            require("oil").set_columns({ "icon" })
+          end
+        end,
+      },
       ["."] = {
         "actions.open_cmdline",
         opts = {
@@ -26,7 +37,7 @@ return {
         end,
         mode = "n",
         nowait = true,
-        desc = "Yank cursor entry full path."
+        desc = "Yank cursor entry full path.",
       },
     },
   },
