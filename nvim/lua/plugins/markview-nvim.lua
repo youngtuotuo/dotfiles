cb14633aa40039a519a4da413aef438400258fd1
Mike Hung <mike@powerarena.com>
2024-08-29 17:41:46 +0800

    markview

--- nvim/lua/plugins/markview-nvim.lua
+++ nvim/lua/plugins/markview-nvim.lua
@@ -1,8 +1,19 @@
 return {
   "OXY2DEV/markview.nvim",
-  lazy = false, -- Recommended
-
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
+  opts = {
+    modes = { "n", "c", "no" },
+    hybrid_modes = { "n", "i" },
+    callbacks = {
+      on_enable = function(_, win)
+        vim.wo[win].conceallevel = 2
+        vim.wo[win].concealcursor = "c"
+      end,
+    },
+  },
+  config = function(_, opts)
+    require("markview").setup(opts)
+  end
 }
