c38b585befae1f5bc37cc410e2f283866939f223
youngtuotuo <youngtuotuo@protonmail.com>
2024-08-31 16:13:14 +0800

    default to powershell

--- nvim/lua/plugins/config.lua
+++ nvim/lua/plugins/config.lua
@@ -200,17 +200,13 @@ vim.g.netrw_hide = 0
 vim.g.netrw_sizestyle = "h"
 
 if vim.fn.has("win32") == 1 then
-  if vim.fn.executable("nu") == 1 then
-    vim.opt.shell = "nu"
-  else
-    vim.opt.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
-    vim.opt.shellcmdflag =
-      "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-    vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
-    vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-    vim.opt.shellquote = ""
-    vim.opt.shellxquote = ""
-  end
+  vim.opt.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
+  vim.opt.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+  vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
 end
 
 vim.filetype.add({
