1bfdd96d972aa7afddc4c670c271e48702811cb0
youngtuotuo <youngtuotuo@protonmail.com>
2024-09-10 22:18:59 +0800

    win32 color

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -1,6 +1,7 @@
 local has_256_colors = (string.find(vim.api.nvim_list_uis()[1].term_name, "xterm%-256color") ~= nil)
   or (string.find(vim.api.nvim_list_uis()[1].term_name, "tmux%-256color") ~= nil)
   or (string.find(vim.api.nvim_list_uis()[1].term_name, "alacritty") ~= nil)
+  or (vim.fn.has("win32") == 1)
 return {
   {
     "norcalli/nvim-colorizer.lua",
