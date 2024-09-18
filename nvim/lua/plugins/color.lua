91f6f471bc4cf42773fb8058e21985fe7d058557
youngtuotuo <youngtuotuo@protonmail.com>
2024-09-18 16:46:26 +0800

    catppuccin tmux

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -23,17 +23,6 @@ return {
     end,
   },
   {
-    "tiagovla/tokyodark.nvim",
-    lazy = true,
-    opts = {
-      -- custom options here
-    },
-    config = function(_, opts)
-      require("tokyodark").setup(opts) -- calling setup is optional
-      vim.cmd([[colorscheme tokyodark]])
-    end,
-  },
-  {
     "jacoborus/tender.vim",
     lazy = has_256_colors,
     config = function()
