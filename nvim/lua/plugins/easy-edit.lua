733222ff44b6d8d7a1652ff3dac9cbade108edb5
Mike Hung <mike@powerarena.com>
2024-10-28 12:14:00 +0800

    default

--- nvim/lua/plugins/easy-edit.lua
+++ nvim/lua/plugins/easy-edit.lua
@@ -22,18 +22,6 @@ return {
     "tpope/vim-vinegar",
   },
   {
-    "chrisgrieser/nvim-spider",
-    opts = {
-      skipInsignificantPunctuation = false,
-    },
-    config = function(_, opts)
-      require("spider").setup(opts)
-      vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
-      vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
-      vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
-    end,
-  },
-  {
     "chrisgrieser/nvim-various-textobjs",
     opts = { useDefaultKeymaps = false },
     config = function(_, opts)
