3f05f829097971e287ba8119b1700a75912a0508
Mike Hung <mike@powerarena.com>
2024-10-29 14:35:03 +0800

    vimgrep is enough

--- nvim/lua/plugins/easy-edit.lua
+++ nvim/lua/plugins/easy-edit.lua
@@ -1,24 +1,5 @@
 return {
   {
-    "stevearc/aerial.nvim",
-    lazy = true,
-    keys = function()
-      local toggle = function()
-        require("aerial").toggle({ focus = true })
-      end
-      return {
-        { "<space>o", toggle, noremap = true, desc = "Code outline" },
-      }
-    end,
-    opts = {
-      layout = {
-        default_direction = "left",
-        max_width = 30,
-        min_width = 30,
-      },
-    },
-  },
-  {
     "tpope/vim-vinegar",
   },
   {
