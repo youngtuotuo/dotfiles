9809056212081ecb6bd37e1a6e03a546347b22c7
youngtuotuo <youngtuotuo@protonmail.com>
2024-11-06 16:26:34 +0800

    plug install for vim

---
+++ vim/install_plug.sh
@@ -0,0 +1,5 @@
+mkdir -p $HOME/.vim/pack/plug/start
+cd $HOME/.vim/pack/plug/start
+git clone --depth 1 https://github.com/tpope/vim-fugitive &
+git clone --depth 1 https://github.com/tpope/vim-vinegar &
+git clone --depth 1 https://github.com/tpope/vim-commentary &
