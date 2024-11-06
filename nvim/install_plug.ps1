7910e64f90445c0e37a54ef62e05119da11d834d
youngtuotuo <youngtuotuo@protonmail.com>
2024-11-06 18:15:31 +0800

    unpowerscripts

--- nvim/install_plug.ps1
+++ nvim/install_plug.ps1
@@ -1,11 +1,11 @@
 $originalDirectory = Get-Location
-New-Item -ItemType Directory -Path "$HOME\AppData\Local\nvim\pack\plug\start" -Force
-cd $HOME\AppData\Local\nvim\pack\plug\start
-Start-Job -ScriptBlock { git clone --depth 1 https://github.com/kylechui/nvim-surround }
-Start-Job -ScriptBlock { git clone --depth 1 https://github.com/nvim-treesitter/nvim-treesitter }
-Start-Job -ScriptBlock { git clone --depth 1 https://github.com/nvim-treesitter/nvim-treesitter-textobjects }
-Start-Job -ScriptBlock { git clone --depth 1 https://github.com/windwp/nvim-ts-autotag }
-Start-Job -ScriptBlock { git clone --depth 1 https://github.com/chrisgrieser/nvim-various-textobjs }
-Start-Job -ScriptBlock { git clone --depth 1 https://github.com/tpope/vim-fugitive }
-Start-Job -ScriptBlock { git clone --depth 1 https://github.com/tpope/vim-vinegar }
+New-Item -ItemType Directory -Path "$env:LOCALAPPDATA\nvim\pack\plug\start" -Force
+cd $env:LOCALAPPDATA\nvim\pack\plug\start
+git clone --depth 1 https://github.com/kylechui/nvim-surround
+git clone --depth 1 https://github.com/nvim-treesitter/nvim-treesitter
+git clone --depth 1 https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+git clone --depth 1 https://github.com/windwp/nvim-ts-autotag
+git clone --depth 1 https://github.com/chrisgrieser/nvim-various-textobjs
+git clone --depth 1 https://github.com/tpope/vim-fugitive
+git clone --depth 1 https://github.com/tpope/vim-vinegar
 cd $originalDirectory
--- vim/install_plug.ps1
+++ vim/install_plug.ps1
@@ -1,7 +1,7 @@
 $originalDirectory = Get-Location
 New-Item -ItemType Directory -Path "$HOME\vimfiles\pack\plug\start" -Force
 cd $HOME\vimfiles\pack\plug\start
-Start-Job -ScriptBlock { git clone --depth 1 https://github.com/tpope/vim-fugitive }
-Start-Job -ScriptBlock { git clone --depth 1 https://github.com/tpope/vim-vinegar }
-Start-Job -ScriptBlock { git clone --depth 1 https://github.com/tpope/vim-commentary }
+git clone --depth 1 https://github.com/tpope/vim-fugitive
+git clone --depth 1 https://github.com/tpope/vim-vinegar
+git clone --depth 1 https://github.com/tpope/vim-commentary
 cd $originalDirectory
