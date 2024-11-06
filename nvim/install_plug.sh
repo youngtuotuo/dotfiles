c90cc28352d4dcb7a264120058346c993982f4ce
youngtuotuo <youngtuotuo@protonmail.com>
2024-11-06 16:03:25 +0800

    plug install scripts

---
+++ nvim/install_plug.sh
@@ -0,0 +1,9 @@
+mkdir -p $HOME/.config/nvim/pack/plug/start
+cd $HOME/.config/nvim/pack/plug/start
+git clone --depth 1 https://github.com/kylechui/nvim-surround &
+git clone --depth 1 https://github.com/nvim-treesitter/nvim-treesitter &
+git clone --depth 1 https://github.com/nvim-treesitter/nvim-treesitter-textobjects &
+git clone --depth 1 https://github.com/windwp/nvim-ts-autotag &
+git clone --depth 1 https://github.com/chrisgrieser/nvim-various-textobjs &
+git clone --depth 1 https://github.com/tpope/vim-fugitive &
+git clone --depth 1 https://github.com/tpope/vim-vinegar &
