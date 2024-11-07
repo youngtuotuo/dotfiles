9bafeed73c427a1f642d43e8e03f9d55130670d9
Mike Hung <mike@powerarena.com>
2024-11-07 15:25:14 +0800

    remove unused

--- nvim/init.lua
+++ nvim/init.lua
@@ -93,14 +93,6 @@ require("nvim-treesitter.configs").setup({
     },
 })
 require("nvim-surround").setup({})
-require("nvim-ts-autotag").setup({
-    opts = {
-        -- Defaults
-        enable_close = true, -- Auto close tags
-        enable_rename = true, -- Auto rename pairs of tags
-        enable_close_on_slash = true, -- Auto close on trailing </
-    },
-})
 
 -- print(vim.inspect(v))
 P = function(v)
