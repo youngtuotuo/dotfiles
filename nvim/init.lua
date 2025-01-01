e654b514ee4d939ff017c45a43479bd59e9022a2
Tuo Hung <youngtuotuo@protonmail.com>
2025-01-01 14:40:07 +0800

    cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-vim.g.colors_name = "deafult"
+vim.cmd.colo [[vividchalk]]
 vim.opt.statusline = [[%<%f %h%w%m%r  - %{g:colors_name} - %{FugitiveStatusline()}%=%-14.(%l,%c%V%) %P]]
 vim.opt.completeopt = [[menu,preview,fuzzy]]
 vim.opt.fillchars:append("vert:|,fold:-,eob:~")
@@ -28,7 +28,6 @@ vim.keymap.set({ "n" }, "J", "mzJ`z", { noremap = true })
 vim.keymap.set({ "v" }, "p", [["_dP]], { noremap = true })
 vim.keymap.set({ "v" }, "<", "<gv", { noremap = true })
 vim.keymap.set({ "v" }, ">", ">gv", { noremap = true })
-vim.keymap.set({ "n" }, "-", "<cmd>Ex<cr>", { silent=true, noremap = true })
 
 local group = vim.api.nvim_create_augroup("Tuo", { clear = true })
 vim.api.nvim_create_autocmd("BufEnter",
@@ -83,3 +82,106 @@ vim.g.mkdp_open_ip = get_lan_ip()
 vim.g.mkdp_echo_preview_url = 1
 vim.g.mkdp_port = "8085"
 vim.g.mkdp_theme = "light"
+
+-- from tpope/vim-ragtag
+local function is_filetype(ft)
+  local filetypes = vim.split(vim.bo.filetype, ".", true)
+  for _, v in ipairs(filetypes) do
+    if v == ft then
+      return true
+    end
+  end
+  return false
+end
+
+local function get_subtype()
+  local top = vim.fn.getline(1) .. "\n" .. vim.fn.getline(2)
+  local ft = vim.bo.filetype
+
+  if (string.find(top, "<?xml>") and not string.match(ft:lower(), "html")) 
+    or string.match(ft:lower(), "^(xml|xsd|xslt|docbk)$") then
+    return "xml"
+  elseif string.match(top:lower(), "<xhtml>") then
+    return "xhtml"
+  elseif string.match(top:lower(), "<!DOCTYPE html>") then
+    return "html5"
+  elseif string.match(top:lower(), "[^<]<html>") then
+    return "html"
+  elseif is_filetype("xhtml") then
+    return "xhtml"
+  else
+    return ""
+  end
+end
+
+
+-- Helper functions for style and script tags
+local function stylesheet_type()
+  if get_subtype() == "html5" then
+    return ""
+  else
+    return ' type="text/css"'
+  end
+end
+
+local function javascript_type()
+  if get_subtype() == "html5" then
+    return ""
+  else
+    return ' type="text/javascript"'
+  end
+end
+
+-- Main function to get tag extras
+local function tag_extras()
+  local subtype = get_subtype()
+  local register = vim.fn.getreg('"')
+
+  if subtype == "xml" then
+    return ""
+  elseif register == "html" and subtype == "xhtml" then
+    local lang = "en"
+    local env_lang = vim.env.LANG
+    if env_lang and string.match(env_lang, "^..") then
+      lang = string.sub(env_lang, 1, 2)
+    end
+    return string.format(' xmlns="http://www.w3.org/1999/xhtml" lang="%s" xml:lang="%s"', lang, lang)
+  elseif register == "style" then
+    return stylesheet_type()
+  elseif register == "script" then
+    return javascript_type()
+  elseif register == "table" then
+    return ' cellspacing="0"'
+  else
+    return ""
+  end
+end
+
+-- Set up the keymaps
+local opts = { noremap = true, silent = true }
+
+-- Helper function to wrap text in tags
+local function wrap_in_tag()
+  local reg = vim.fn.getreg('"')
+  local extras = tag_extras()
+  return string.format("<%s%s></%s>", reg, extras, reg)
+end
+
+-- <C-X><Space> mapping
+vim.keymap.set('i', '<C-X><Space>', function()
+  vim.cmd('normal! ciw')
+  local wrapped = wrap_in_tag()
+  vim.api.nvim_put({wrapped}, 'c', false, true)
+  vim.cmd('normal! F<i')
+end, opts)
+
+-- <C-X><CR> mapping
+vim.keymap.set('i', '<C-X><CR>', function()
+  vim.cmd('normal! ciw')
+  local wrapped = wrap_in_tag()
+  local lines = vim.split(wrapped, '><')
+  lines[1] = lines[1] .. '>'
+  lines[2] = '<' .. lines[2]
+  vim.api.nvim_put(lines, 'l', false, true)
+  vim.cmd('normal! k$o')
+end, opts)
--- vim/install_plug.sh
+++ vim/install_plug.sh
@@ -10,13 +10,17 @@ plugins=(
 		"https://github.com/mzlogin/vim-markdown-toc"
 		"https://github.com/sheerun/vim-polyglot"
 		"https://github.com/tommcdo/vim-exchange"
+		"https://github.com/tpope/vim-abolish"
 		"https://github.com/tpope/vim-commentary"
 		"https://github.com/tpope/vim-endwise"
 		"https://github.com/tpope/vim-fugitive"
+		"https://github.com/tpope/vim-repeat"
+		"https://github.com/tpope/vim-rsi"
 		"https://github.com/tpope/vim-surround"
 		"https://github.com/tpope/vim-unimpaired"
+		"https://github.com/tpope/vim-vinegar"
+		"https://github.com/tpope/vim-vividchalk"
 		"https://github.com/wellle/targets.vim"
-		"https://github.com/youngtuotuo/pure.vim"
 )
 
 packpath=$HOME/.config/vim/pack/plug/start
