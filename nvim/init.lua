3513a8f0b321bbbc4d23287a77a9f90d901f9934
youngtuotuo <youngtuotuo@protonmail.com>
2025-01-07 12:10:46 +0800

    keep small

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,28 +1,3 @@
-vim.cmd.colo [[vividchalk]]
-local function indent_info()
-  local et = vim.opt.expandtab:get() and 'et' or 'noet'
-  return string.format("[tw=%d:sw=%d:ts=%d:%s:ft=%s]",
-    vim.opt.textwidth:get(),
-    vim.opt.shiftwidth:get(),
-    vim.opt.tabstop:get(),
-    et,
-    vim.bo.filetype)
-end
-vim.opt.statusline = [[%<%f %h%w%m%r  *%{g:colors_name}*   ]] .. indent_info() .. [[  %{FugitiveStatusline()}%= %-14.(%l,%c%V%) %P]]
-vim.opt.completeopt = [[menu,preview,fuzzy]]
-vim.opt.fillchars:append("vert:|,fold:-,eob:~")
-vim.opt.foldopen:remove([[block]])
-vim.opt.inccommand = "split"
-vim.opt.laststatus = 2
-vim.opt.listchars:append([[eol:$]])
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.shada = { "'10", "<0", "s10", "h" }
-vim.opt.shiftwidth = 4
-vim.opt.showmatch = true
-vim.opt.smartindent = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
 vim.opt.swapfile = false
 vim.opt.undofile = true
 vim.opt.undodir = vim.fn.stdpath("state") .. "/undo"
@@ -34,9 +9,6 @@ vim.keymap.set({ "i" }, ".", "<C-g>u.", { noremap = true })
 vim.keymap.set({ "t" }, "<esc><esc>", "<C-\\><C-n>", { noremap = true })
 vim.keymap.set({ "n" }, "Y", "y$", { noremap = true })
 vim.keymap.set({ "n" }, "J", "mzJ`z", { noremap = true })
-vim.keymap.set({ "v" }, "p", [["_dP]], { noremap = true })
-vim.keymap.set({ "v" }, "<", "<gv", { noremap = true })
-vim.keymap.set({ "v" }, ">", ">gv", { noremap = true })
 
 local group = vim.api.nvim_create_augroup("Tuo", { clear = true })
 vim.api.nvim_create_autocmd("BufEnter",
@@ -44,14 +16,6 @@ vim.api.nvim_create_autocmd("BufEnter",
     group = group,
     callback = function(args)
       vim.treesitter.stop(args.buf)
-    end
-  }
-)
-vim.api.nvim_create_autocmd("TextYankPost",
-  {
-    group = group,
-    callback = function()
-      vim.highlight.on_yank()
     end
   }
 )
@@ -78,106 +42,3 @@ vim.g.mkdp_open_ip = get_lan_ip()
 vim.g.mkdp_echo_preview_url = 1
 vim.g.mkdp_port = "8085"
 vim.g.mkdp_theme = "light"
-
--- from tpope/vim-ragtag
-local function is_filetype(ft)
-  local filetypes = vim.split(vim.bo.filetype, ".", true)
-  for _, v in ipairs(filetypes) do
-    if v == ft then
-      return true
-    end
-  end
-  return false
-end
-
-local function get_subtype()
-  local top = vim.fn.getline(1) .. "\n" .. vim.fn.getline(2)
-  local ft = vim.bo.filetype
-
-  if (string.find(top, "<?xml>") and not string.match(ft:lower(), "html")) 
-    or string.match(ft:lower(), "^(xml|xsd|xslt|docbk)$") then
-    return "xml"
-  elseif string.match(top:lower(), "<xhtml>") then
-    return "xhtml"
-  elseif string.match(top:lower(), "<!DOCTYPE html>") then
-    return "html5"
-  elseif string.match(top:lower(), "[^<]<html>") then
-    return "html"
-  elseif is_filetype("xhtml") then
-    return "xhtml"
-  else
-    return ""
-  end
-end
-
-
--- Helper functions for style and script tags
-local function stylesheet_type()
-  if get_subtype() == "html5" then
-    return ""
-  else
-    return ' type="text/css"'
-  end
-end
-
-local function javascript_type()
-  if get_subtype() == "html5" then
-    return ""
-  else
-    return ' type="text/javascript"'
-  end
-end
-
--- Main function to get tag extras
-local function tag_extras()
-  local subtype = get_subtype()
-  local register = vim.fn.getreg('"')
-
-  if subtype == "xml" then
-    return ""
-  elseif register == "html" and subtype == "xhtml" then
-    local lang = "en"
-    local env_lang = vim.env.LANG
-    if env_lang and string.match(env_lang, "^..") then
-      lang = string.sub(env_lang, 1, 2)
-    end
-    return string.format(' xmlns="http://www.w3.org/1999/xhtml" lang="%s" xml:lang="%s"', lang, lang)
-  elseif register == "style" then
-    return stylesheet_type()
-  elseif register == "script" then
-    return javascript_type()
-  elseif register == "table" then
-    return ' cellspacing="0"'
-  else
-    return ""
-  end
-end
-
--- Set up the keymaps
-local opts = { noremap = true, silent = true }
-
--- Helper function to wrap text in tags
-local function wrap_in_tag()
-  local reg = vim.fn.getreg('"')
-  local extras = tag_extras()
-  return string.format("<%s%s></%s>", reg, extras, reg)
-end
-
--- <C-X><Space> mapping
-vim.keymap.set('i', '<C-X><Space>', function()
-  vim.cmd('normal! ciw')
-  local wrapped = wrap_in_tag()
-  vim.api.nvim_put({wrapped}, 'c', false, true)
-  vim.cmd('normal! F<i')
-end, opts)
-
--- <C-X><CR> mapping
-vim.keymap.set('i', '<C-X><CR>', function()
-  vim.cmd('normal! ciw')
-  local wrapped = wrap_in_tag()
-  local lines = vim.split(wrapped, '><')
-  lines[1] = lines[1] .. '>'
-  lines[2] = '<' .. lines[2]
-  vim.api.nvim_put(lines, 'l', false, true)
-  vim.cmd('normal! k$o')
-end, opts)
--- nvim/install_plug.sh
+++ nvim/install_plug.sh
@@ -1,51 +1,42 @@
 #!/usr/bin/env bash
 
 plugins=(
-		"https://github.com/iamcco/markdown-preview.nvim"
-		"https://github.com/junegunn/fzf"
-		"https://github.com/junegunn/vim-easy-align"
-		"https://github.com/kaarmu/typst.vim"
-		"https://github.com/mbbill/undotree"
-		"https://github.com/mzlogin/vim-markdown-toc"
-		"https://github.com/sheerun/vim-polyglot"
-		"https://github.com/tommcdo/vim-exchange"
-		"https://github.com/tpope/vim-abolish"
-		"https://github.com/tpope/vim-apathy"
-		"https://github.com/tpope/vim-characterize"
-		"https://github.com/tpope/vim-commentary"
-		"https://github.com/tpope/vim-endwise"
-		"https://github.com/tpope/vim-eunuch"
-		"https://github.com/tpope/vim-fugitive"
-		"https://github.com/tpope/vim-repeat"
-		"https://github.com/tpope/vim-rsi"
-		"https://github.com/tpope/vim-scriptease"
-		"https://github.com/tpope/vim-speeddating"
-		"https://github.com/tpope/vim-surround"
-		"https://github.com/tpope/vim-unimpaired"
-		"https://github.com/tpope/vim-vinegar"
-		"https://github.com/wellle/targets.vim"
+	"https://github.com/iamcco/markdown-preview.nvim"
+	"https://github.com/junegunn/fzf"
+	"https://github.com/junegunn/vim-easy-align"
+	"https://github.com/kaarmu/typst.vim"
+	"https://github.com/mbbill/undotree"
+	"https://github.com/sheerun/vim-polyglot"
+	"https://github.com/tommcdo/vim-exchange"
+	"https://github.com/tpope/vim-commentary"
+	"https://github.com/tpope/vim-endwise"
+	"https://github.com/tpope/vim-fugitive"
+	"https://github.com/tpope/vim-rsi"
+	"https://github.com/tpope/vim-surround"
+	"https://github.com/tpope/vim-unimpaired"
+	"https://github.com/tpope/vim-vinegar"
+	"https://github.com/wellle/targets.vim"
 )
-
 packpath=$HOME/.config/nvim/pack/plug/start
 
 if [ ! -d $packpath ]; then
-    mkdir -p $packpath
+	mkdir -p $packpath
 fi
 
 cd $packpath
 
 function set_plugin() {
-    repo_name=$(basename "$1")
-    plugin_dir=$packpath/$repo_name
-    if [ ! -d "$plugin_dir" ]; then
-        git clone --depth 1 $1
-        if [ -d "$repo_name/doc" ]; then
-            nvim --headless -u NONE "+helptags $repo_name/doc" +qa
-        fi
-    fi
+	repo_name=$(basename "$1")
+	plugin_dir=$packpath/$repo_name
+	if [ ! -d "$plugin_dir" ]; then
+		git clone --depth 1 $1
+		if [ -d "$repo_name/doc" ]; then
+			nvim --headless -u NONE "+helptags $repo_name/doc" +qa
+		fi
+	fi
 }
 
 for url in "${plugins[@]}"; do
-    set_plugin "$url" &
+	set_plugin "$url" &
 done
 wait
