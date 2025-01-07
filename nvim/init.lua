13bbf1b66ed315c353f5807c342484fec50cd69c
Tuo Hung <youngtuotuo@protonmail.com>
2025-01-08 07:55:42 +0800

    less plug

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,24 +23,3 @@ vim.api.nvim_create_autocmd("BufEnter",
 )
 
 vim.cmd.packadd [[cfilter]]
-
-vim.g.fzf_layout = { down = [[40%]] }
-
-local get_lan_ip = function()
-  if vim.fn.has("mac") == 1 then
-    local cmd = "ipconfig getifaddr en0"
-    local ip = vim.fn.system(cmd):match("([%d%.]+)%\n")
-    return ip
-  elseif vim.fn.has("linux") == 1 or vim.fn.has("wsl") == 1 then
-    local cmd = "ip route get 1.1.1.1 | awk '{print $7}'"
-    local ip = vim.fn.system(cmd)
-    return ip:gsub("%s+", "") -- Remove any leading/trailing whitespace
-  end
-end
-
-vim.g.mkdp_auto_close = 0
-vim.g.mkdp_open_to_the_world = 1
-vim.g.mkdp_open_ip = get_lan_ip()
-vim.g.mkdp_echo_preview_url = 1
-vim.g.mkdp_port = "8085"
-vim.g.mkdp_theme = "light"
