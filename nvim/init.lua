0ab5efd51363b3aab95d3e4173c10be7feedb7f7
Mike Hung <mike@powerarena.com>
2025-01-08 16:54:31 +0800

    bootstrap vim-plug

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,3 +23,62 @@ vim.api.nvim_create_autocmd("BufEnter",
 )
 
 vim.cmd.packadd [[cfilter]]
+
+-- vim-plug bootstrap installation script for Neovim
+local fn = vim.fn
+local install_path = '~/.config/nvim/autoload/plug.vim'
+
+-- Check if vim-plug is installed
+local function ensure_plug()
+    if fn.empty(fn.glob(install_path)) > 0 then
+        -- Create required directories
+        fn.mkdir('~/.config/nvim/autoload', 'p')
+
+        -- Define installation command
+        local install_cmd = string.format(
+            'curl -fLo %s --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim',
+            install_path
+        )
+
+        -- Execute installation command
+        print("Installing vim-plug...")
+        local output = fn.system(install_cmd)
+
+        -- Check installation status
+        if vim.v.shell_error == 0 then
+            print("vim-plug installed successfully!")
+            -- Schedule PlugInstall to run after vim-plug is loaded
+            vim.cmd([[
+                autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+            ]])
+        else
+            error("Failed to install vim-plug: " .. output)
+            return false
+        end
+    end
+    return true
+end
+
+local function init_plug()
+    if ensure_plug() then
+      local vim = vim
+      local Plug = vim.fn['plug#']
+      vim.call('plug#begin')
+      Plug('https://github.com/junegunn/vim-easy-align')
+      Plug('https://github.com/kaarmu/typst.vim')
+      Plug('https://github.com/mbbill/undotree')
+      Plug('https://github.com/sheerun/vim-polyglot')
+      Plug('https://github.com/tommcdo/vim-exchange')
+      Plug('https://github.com/tpope/vim-apathy')
+      Plug('https://github.com/tpope/vim-commentary')
+      Plug('https://github.com/tpope/vim-endwise')
+      Plug('https://github.com/tpope/vim-fugitive')
+      Plug('https://github.com/tpope/vim-rsi')
+      Plug('https://github.com/tpope/vim-surround')
+      Plug('https://github.com/tpope/vim-vinegar')
+      Plug('https://github.com/wellle/targets.vim')
+      vim.call('plug#end')
+    end
+end
+
+init_plug()
