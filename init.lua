6dcb899a15fabaca403c9d15262e8a8c4f88cbee
Mike Hung <mike@powerarena.com>
2025-04-15 14:12:07 +0800

    clean

--- init.lua
+++ init.lua
@@ -5,7 +5,13 @@ vim.opt.hlsearch = false
 vim.opt.undofile = true
 vim.opt.wrap = false
 vim.opt.undodir = vim.fn.stdpath("state") .. "/undo"
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 vim.o.winborder = "rounded"
+
+vim.fn.matchadd("ExtraWhitespace", [[\s\+$]])
+vim.api.nvim_set_hl(0, "ExtraWhitespace", { ctermbg = 9, bg = "NvimLightRed"})
 
 vim.keymap.set({ "i" }, ",", "<C-g>u,", { noremap = true })
 vim.keymap.set({ "i" }, ".", "<C-g>u.", { noremap = true })
@@ -15,15 +21,17 @@ vim.cmd.packadd [[cfilter]]
 
 vim.lsp.config.basedpyright = {
 	cmd = { "basedpyright-langserver", "--stdio" },
-	root_markers = { "pyproject.toml", ".git" },
+	root_markers = { "ruff.toml", ".git" },
 	filetypes = { "python" },
+	offset_encoding = "utf-8",
 	settings = {
 		basedpyright = {
 			analysis = {
 				autoImportCompletions = true,
 				autoSearchPaths = true,
 				useLibraryCodeForTypes = true,
-				ignore = { "*" }
+				diagnosticMode = "openFilesOnly",
+                                ignore = { "*" }
 			},
 			disableOrganizeImports = true
 		},
@@ -35,13 +43,15 @@ vim.lsp.config.ruff = {
 	cmd = { 'ruff', 'server' },
 	root_markers = { 'ruff.toml', '.git' },
 	filetypes = { "python" },
-	settings = {
-		lineLength = 150,
-		lint = {
-			ignore = { "E701", "E402", "E712" }
+	init_options = {
+		settings = {
+			lineLength = 150,
+			lint = {
+				ignore = { "E701", "E402", "E712" }
+			}
 		}
 	}
 }
 vim.lsp.enable('ruff')
 
-vim.diagnostic.config({ virtual_lines = { current_line = false } })
+vim.diagnostic.config({ virtual_text = { source = true } })
