2d0f2cc9eb93ac17757149062b5448f9797f10c5
Tuo Hung <youngtuotuo@protonmail.com>
2025-07-03 10:31:52 +0800

    python help me decide what to learn

---
+++ tpchser.py
@@ -0,0 +1,16 @@
+import random
+
+topics = [
+    "Leetcode",
+    "Computer System A Programmer's Perspective",
+    "Bishop Deep Learning",
+    "Linux From Scratch",
+    "Any Paper",
+    "Any ML Implementation",
+    "Computer Enhance",
+    "A Math A day",
+    "Any bookmark",
+    "X bookmark"
+]
+
+print(random.choice(topics))
